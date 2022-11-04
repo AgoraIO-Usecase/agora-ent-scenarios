6f4808619792f1f4b9485657ab7b09b264580b3b
xucz <xgfd3@users.noreply.github.com>
2022-11-04 12:00:03 +0800

    [Android]add service protocol and perfect the code in room create part.

--- Android/scenes/ktv/src/main/java/io/agora/scene/ktv/create/RoomCreateViewModel.java
+++ Android/scenes/ktv/src/main/java/io/agora/scene/ktv/create/RoomCreateViewModel.java
@@ -1,26 +1,40 @@
 package io.agora.scene.ktv.create;
 
+import android.app.Application;
+
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.MutableLiveData;
+
+import java.util.List;
 
 import io.agora.scene.base.KtvConstant;
-import io.agora.scene.base.api.ApiException;
-import io.agora.scene.base.api.ApiManager;
-import io.agora.scene.base.api.ApiSubscriber;
-import io.agora.scene.base.api.apiutils.SchedulersUtil;
-import io.agora.scene.base.api.base.BaseResponse;
-import io.agora.scene.base.bean.RoomListModel;
-import io.agora.scene.base.component.BaseRequestViewModel;
 import io.agora.scene.base.data.model.AgoraRoom;
 import io.agora.scene.base.manager.RTMManager;
 import io.agora.scene.base.manager.RoomManager;
 import io.agora.scene.base.manager.UserManager;
 import io.agora.scene.base.utils.ToastUtils;
-import io.reactivex.disposables.Disposable;
+import io.agora.scene.ktv.service.KTVCreateRoomInputModel;
+import io.agora.scene.ktv.service.KTVJoinRoomInputModel;
+import io.agora.scene.ktv.service.KTVServiceImp;
+import io.agora.scene.ktv.service.KTVServiceProtocol;
+import io.agora.scene.ktv.service.VLRoomListModel;
 
-public class RoomCreateViewModel extends BaseRequestViewModel {
-    private int current = 1;
-    private int size = 30;
+public class RoomCreateViewModel extends AndroidViewModel {
+    public final static int ERROR_PASSWORD = 20009;
+
+    private final KTVServiceProtocol ktvServiceProtocol;
+
+
+    public final MutableLiveData<List<VLRoomListModel>> roomModelList = new MutableLiveData<>();
+    public final MutableLiveData<Boolean> joinRoomResult = new MutableLiveData<>();
+    public final MutableLiveData<Boolean> createRoomResult = new MutableLiveData<>();
+
+
+    public RoomCreateViewModel(@NonNull Application application) {
+        super(application);
+        ktvServiceProtocol = new KTVServiceImp();
+    }
 
     public void loginRTM() {
         RTMManager.getInstance().doLoginRTM();
@@ -34,115 +48,73 @@ public class RoomCreateViewModel extends BaseRequestViewModel {
      * 加载房间列表
      */
     public void loadRooms() {
-        ApiManager.getInstance().requestRoomList(current, size)
-                .compose(SchedulersUtil.INSTANCE.applyApiSchedulers()).subscribe(
-                        new ApiSubscriber<BaseResponse<RoomListModel>>() {
-                            @Override
-                            public void onSubscribe(@NonNull Disposable d) {
-                                addDispose(d);
-                            }
-
-                            @Override
-                            public void onSuccess(BaseResponse<RoomListModel> data) {
-                                getISingleCallback().onSingleCallback(KtvConstant.CALLBACK_TYPE_ROOM_GET_ROOM_LIST_SUCCESS
-                                        , data.getData().records);
-
-                            }
-
-                            @Override
-                            public void onFailure(@Nullable ApiException t) {
-                                //创建失败
-                                getISingleCallback().onSingleCallback(KtvConstant.CALLBACK_TYPE_ROOM_GET_ROOM_LIST_FAIL, null);
-                                ToastUtils.showToast(t.getMessage());
-                            }
-                        }
-                );
+        ktvServiceProtocol.getRoomListWithPage((e, vlRoomListModels) -> {
+            if (e != null) {
+                ToastUtils.showToast(e.getMessage());
+                roomModelList.postValue(null);
+                return null;
+            }
+            roomModelList.postValue(vlRoomListModels);
+            return null;
+        });
     }
 
-    public void getRoomToken(String roomNo, String password) {
-        ApiManager.getInstance().requestGetRoomInfo(roomNo, password)
-                .compose(SchedulersUtil.INSTANCE.applyApiSchedulers()).subscribe(
-                        new ApiSubscriber<BaseResponse<AgoraRoom>>() {
-                            @Override
-                            public void onSubscribe(@NonNull Disposable d) {
-                                addDispose(d);
-                            }
+    public void createRoom(int isPrivate,
+                           String name, String password,
+                           String userNo, String icon) {
+        ktvServiceProtocol.createRoomWithInput(new KTVCreateRoomInputModel(
+                "", icon, isPrivate, name, password, "", userNo
+        ), (e, ktvCreateRoomOutputModel) -> {
+            if (e == null && ktvCreateRoomOutputModel != null) {
+                // success
+                AgoraRoom room = new AgoraRoom();
+                room.roomNo = ktvCreateRoomOutputModel.getRoomNo();
+                room.creatorNo = UserManager.getInstance().getUser().userNo;
+                room.isPrivate = isPrivate;
+                room.name = name;
+                room.belCanto = "0";
+                room.icon = icon;
+                // TODO remove RoomManager. Do not save room info, but just pass it to living room.
+                RoomManager.getInstance().setAgoraRoom(room);
 
-                            @Override
-                            public void onSuccess(BaseResponse<AgoraRoom> data) {
-                                KtvConstant.RTM_TOKEN = data.getData().agoraRTMToken;
-                                KtvConstant.RTC_TOKEN = data.getData().agoraRTCToken;
-                                KtvConstant.PLAYER_TOKEN = data.getData().agoraPlayerRTCToken;
-                                loginRTM();
-                                if (isCreator) {
-                                    //创建成功 直接加入房间
-                                    getISingleCallback().onSingleCallback(KtvConstant.CALLBACK_TYPE_ROOM_CREATE_SUCCESS, null);
-                                    ToastUtils.showToast("创建房间成功");
-                                    isCreator = false;
-                                } else {
-                                    getISingleCallback().onSingleCallback(KtvConstant.CALLBACK_TYPE_ROOM_RTM_RTC_TOKEN, null);
-                                }
+                // TODO login RTM. We should save the token?
+                KtvConstant.RTM_TOKEN = ktvCreateRoomOutputModel.getAgoraRTMToken();
+                KtvConstant.RTC_TOKEN = ktvCreateRoomOutputModel.getAgoraRTCToken();
+                KtvConstant.PLAYER_TOKEN = ktvCreateRoomOutputModel.getAgoraPlayerRTCToken();
+                loginRTM();
 
-                            }
-
-                            @Override
-                            public void onFailure(@Nullable ApiException t) {
-                                if (t != null && t.errCode == 20009) {
-                                    getISingleCallback().onSingleCallback(KtvConstant.CALLBACK_TYPE_ROOM_PASSWORD_ERROR, null);
-                                }
-                            }
-                        }
-                );
+                createRoomResult.postValue(true);
+            } else {
+                // failed
+                if (e != null) {
+                    ToastUtils.showToast(e.getMessage());
+                }
+                createRoomResult.postValue(false);
+            }
+            return null;
+        });
     }
 
-    private boolean isCreator = false;
+    public void joinRoom(String roomNo, String password) {
+        ktvServiceProtocol.joinRoomWithInput(new KTVJoinRoomInputModel(roomNo, password), (e, ktvJoinRoomOutputModel) -> {
+            if(e == null && ktvJoinRoomOutputModel != null){
+                // success
 
-    /**
-     * 创建房间
-     *
-     * @param isPrivate 是否是私密 1 是 0 否
-     * @param name      房间名称
-     * @param password  房间密码
-     * @param userNo    用户id
-     * @param icon      icon图
-     */
-    public void requestCreateRoom(int isPrivate, String name,
-                                  String password, String userNo, String icon) {
-        ApiManager.getInstance().requestCreateRoom(isPrivate, name, password, userNo, icon)
-                .compose(SchedulersUtil.INSTANCE.applyApiSchedulers()).subscribe(
-                        new ApiSubscriber<BaseResponse<String>>() {
-                            @Override
-                            public void onSubscribe(@NonNull Disposable d) {
-                                addDispose(d);
-                            }
+                KtvConstant.RTM_TOKEN = ktvJoinRoomOutputModel.getAgoraRTMToken();
+                KtvConstant.RTC_TOKEN = ktvJoinRoomOutputModel.getAgoraRTCToken();
+                KtvConstant.PLAYER_TOKEN = ktvJoinRoomOutputModel.getAgoraPlayerRTCToken();
+                loginRTM();
 
-                            @Override
-                            public void onSuccess(BaseResponse<String> data) {
-                                AgoraRoom room = new AgoraRoom();
-                                room.roomNo = data.getData();
-                                room.creatorNo = UserManager.getInstance().getUser().userNo;
-                                room.isPrivate = isPrivate;
-                                room.name = name;
-                                room.belCanto = "0";
-                                room.icon = icon;
-//                                if (isPrivate == 1) {
-//                                    room.password = password;
-//                                }
-                                RoomManager.getInstance().setAgoraRoom(room);
-
-                                isCreator = true;
-                                getRoomToken(room.roomNo, null);
-                            }
-
-                            @Override
-                            public void onFailure(@Nullable ApiException t) {
-                                //创建失败
-                                getISingleCallback().onSingleCallback(KtvConstant.CALLBACK_TYPE_ROOM_CREATE_FAIL, null);
-                                ToastUtils.showToast(t.getMessage());
-                            }
-                        }
-                );
+                joinRoomResult.postValue(true);
+            } else {
+                // failed
+                if (e != null) {
+                    ToastUtils.showToast(e.getMessage());
+                }
+                joinRoomResult.postValue(false);
+            }
+            return null;
+        });
     }
-
 
 }
