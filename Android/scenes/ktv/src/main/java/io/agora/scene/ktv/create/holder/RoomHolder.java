5efbc4115aa8f8b25aa5fe0530a6f6a86310dc21
xucz <xgfd3@users.noreply.github.com>
2022-11-04 12:00:03 +0800

    [Android]add service protocol and perfect the code in room create part.

--- Android/scenes/ktv/src/main/java/io/agora/scene/ktv/create/holder/RoomHolder.java
+++ Android/scenes/ktv/src/main/java/io/agora/scene/ktv/create/holder/RoomHolder.java
@@ -6,25 +6,25 @@ import androidx.annotation.NonNull;
 
 import io.agora.scene.base.GlideApp;
 import io.agora.scene.base.component.BaseRecyclerViewAdapter;
-import io.agora.scene.base.data.model.AgoraRoom;
 import io.agora.scene.ktv.R;
 import io.agora.scene.ktv.databinding.ItemRoomListBinding;
+import io.agora.scene.ktv.service.VLRoomListModel;
 import io.agora.scene.widget.utils.CenterCropRoundCornerTransform;
 
-public class RoomHolder extends BaseRecyclerViewAdapter.BaseViewHolder<ItemRoomListBinding, AgoraRoom> {
+public class RoomHolder extends BaseRecyclerViewAdapter.BaseViewHolder<ItemRoomListBinding, VLRoomListModel> {
 
     public RoomHolder(@NonNull ItemRoomListBinding mBinding) {
         super(mBinding);
     }
 
     @Override
-    public void binding(AgoraRoom data, int selectedIndex) {
+    public void binding(VLRoomListModel data, int selectedIndex) {
         if (data != null) {
             GlideApp.with(mBinding.ivRoomCover.getContext()).load(data.getCoverRes())
                     .transform(new CenterCropRoundCornerTransform(40)).into(mBinding.ivRoomCover);
-            mBinding.tvRoomName.setText(data.name);
-            mBinding.tvPersonNum.setText(String.format("%d%s", data.roomPeopleNum, itemView.getContext().getString(R.string.people)));
-            if (data.isPrivate == 1){
+            mBinding.tvRoomName.setText(data.getName());
+            mBinding.tvPersonNum.setText(String.format("%d%s", data.getRoomPeopleNum(), itemView.getContext().getString(R.string.people)));
+            if (data.isPrivate()){
                 mBinding.ivLock.setVisibility(View.VISIBLE);
             } else{
                 mBinding.ivLock.setVisibility(View.GONE);
