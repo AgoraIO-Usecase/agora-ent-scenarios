e49338ce982285ed422c00c46fe891bdcf84b880
wushengtao <tamworth@163.com>
2022-11-08 20:39:02 +0800

    refactor: fix compile fail

--- iOS/AgoraEntScenarios/Scenes/KTV/View/KTV/View/VLBadNetWorkView.h
+++ iOS/AgoraEntScenarios/Scenes/KTV/View/KTV/View/VLBadNetWorkView.h
@@ -6,6 +6,7 @@
 #import <UIKit/UIKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
+@class VLBadNetWorkView;
 @protocol VLBadNetWorkViewDelegate <NSObject>
 
 - (void)onVLBadNetworkView:(VLBadNetWorkView*)view dismiss:(id)sender;
