7b2e94ca2fe6de1e292307511fe7c615c5badf55
卫 张 <zhangwei03@agora.io>
2022-11-11 16:10:43 +0800

    [Android-VoiceChat] delete code

--- Android/app/src/main/java/com/agora/entfulldemo/home/HomeIndexFragment.java
+++ Android/app/src/main/java/com/agora/entfulldemo/home/HomeIndexFragment.java
@@ -1,7 +1,6 @@
 package com.agora.entfulldemo.home;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.graphics.Rect;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,22 +8,17 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.core.content.ContextCompat;
-import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.agora.entfulldemo.R;
-import com.agora.entfulldemo.databinding.AppFragmentHomeIndexNewBinding;
-import com.agora.entfulldemo.databinding.AppItemHomeIndexBinding;
+import com.agora.entfulldemo.databinding.FragmentHomeIndexBinding;
+import com.agora.entfulldemo.databinding.ItemHomeIndexBinding;
 import com.agora.entfulldemo.home.constructor.ScenesConstructor;
 import com.agora.entfulldemo.home.constructor.ScenesModel;
 import com.agora.entfulldemo.home.holder.HomeIndexHolder;
 import com.alibaba.android.arouter.launcher.ARouter;
-import com.google.android.material.divider.MaterialDividerItemDecoration;
 
 import java.util.List;
 
-import io.agora.rtc2.internal.DeviceUtils;
 import io.agora.scene.base.PagePathConstant;
 import io.agora.scene.base.component.BaseRecyclerViewAdapter;
 import io.agora.scene.base.component.BaseViewBindingFragment;
@@ -32,12 +26,12 @@ import io.agora.scene.base.component.OnItemClickListener;
 import io.agora.scene.base.manager.PagePilotManager;
 import io.agora.scene.base.utils.KTVUtil;
 
-public class HomeIndexFragment extends BaseViewBindingFragment<AppFragmentHomeIndexNewBinding> {
+public class HomeIndexFragment extends BaseViewBindingFragment<FragmentHomeIndexBinding> {
 
     @NonNull
     @Override
-    protected AppFragmentHomeIndexNewBinding getViewBinding(@NonNull LayoutInflater inflater, @Nullable ViewGroup container) {
-        return AppFragmentHomeIndexNewBinding.inflate(inflater);
+    protected FragmentHomeIndexBinding getViewBinding(@NonNull LayoutInflater inflater, @Nullable ViewGroup container) {
+        return FragmentHomeIndexBinding.inflate(inflater);
     }
 
     @Override
@@ -46,7 +40,7 @@ public class HomeIndexFragment extends BaseViewBindingFragment<AppFragmentHomeIn
         Context context = getContext();
         if (context != null) {
             List<ScenesModel> scenesModels = ScenesConstructor.buildData(context);
-            BaseRecyclerViewAdapter<AppItemHomeIndexBinding, ScenesModel, HomeIndexHolder> homeIndexAdapter = new BaseRecyclerViewAdapter<>(scenesModels, new OnItemClickListener<ScenesModel>() {
+            BaseRecyclerViewAdapter<ItemHomeIndexBinding, ScenesModel, HomeIndexHolder> homeIndexAdapter = new BaseRecyclerViewAdapter<>(scenesModels, new OnItemClickListener<ScenesModel>() {
                 @Override
                 public void onItemClick(@NonNull ScenesModel scenesModel, View view, int position, long viewType) {
                     if (scenesModel.getActive()) {
@@ -54,9 +48,6 @@ public class HomeIndexFragment extends BaseViewBindingFragment<AppFragmentHomeIn
                     }
                 }
             }, HomeIndexHolder.class);
-            MaterialDividerItemDecoration dividerItemDecorationV = new MaterialDividerItemDecoration(context, MaterialDividerItemDecoration.VERTICAL);
-            dividerItemDecorationV.setDividerThickness(KTVUtil.dp2px(8));
-            dividerItemDecorationV.setDividerColor(Color.TRANSPARENT);
             int padding = KTVUtil.dp2px(8);
             RecyclerView.ItemDecoration itemDecoration = new RecyclerView.ItemDecoration() {
                 @Override
@@ -68,7 +59,6 @@ public class HomeIndexFragment extends BaseViewBindingFragment<AppFragmentHomeIn
                     outRect.right = padding;
                 }
             };
-//            getBinding().rvScenes.addItemDecoration(dividerItemDecorationV);
             getBinding().rvScenes.addItemDecoration(itemDecoration);
             getBinding().rvScenes.setAdapter(homeIndexAdapter);
         }
@@ -97,14 +87,5 @@ public class HomeIndexFragment extends BaseViewBindingFragment<AppFragmentHomeIn
 
     @Override
     public void initListener() {
-//        getBinding().bgGoKTV.setOnClickListener(view -> PagePilotManager.pageRoomList());
-//        getBinding().bgChatting.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                ARouter.getInstance()
-//                        .build(PagePathConstant.pageVoiceSplash)
-//                        .navigation();
-//            }
-//        });
     }
 }
