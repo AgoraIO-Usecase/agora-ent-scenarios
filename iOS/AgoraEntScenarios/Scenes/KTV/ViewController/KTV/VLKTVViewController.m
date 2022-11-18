f18d569535b97c68ae315c5a5557d533ab87c353
wushengtao <tamworth@163.com>
2022-11-18 11:00:48 +0800

    fix: check invalid of play status

--- iOS/AgoraEntScenarios/Scenes/KTV/ViewController/KTV/VLKTVViewController.m
+++ iOS/AgoraEntScenarios/Scenes/KTV/ViewController/KTV/VLKTVViewController.m
@@ -505,6 +505,12 @@ reportAudioVolumeIndicationOfSpeakers:(NSArray<AgoraRtcAudioVolumeInfo *> *)spea
 //                    VLLog(@"发送失败");
                 }
             }];
+            //check invalid
+            if (![self.currentPlayingSongNo isEqualToString:self.selSongsArray.firstObject.songNo]) {
+                KTVLogInfo(@"play fail, current playing songNo: %@, topSongNo: %@", self.currentPlayingSongNo, self.selSongsArray.firstObject.songNo);
+                [self stopCurrentSong];
+                [self loadAndPlaySong];
+            }
         }
 }
 
