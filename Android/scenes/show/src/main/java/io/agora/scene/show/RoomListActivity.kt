e1cde0fd4e285c1e54a1ff8c236ae41b3a2bc49c
xucz <xgfd3@users.noreply.github.com>
2022-11-22 17:53:28 +0800

    [Show][Android]perfect room service logic and ui.

--- Android/scenes/show/src/main/java/io/agora/scene/show/RoomListActivity.kt
+++ Android/scenes/show/src/main/java/io/agora/scene/show/RoomListActivity.kt
@@ -7,7 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import io.agora.scene.show.databinding.ShowRoomItemBinding
 import io.agora.scene.show.databinding.ShowRoomListActivityBinding
-import io.agora.scene.show.service.ShowRoomListModel
+import io.agora.scene.show.service.ShowRoomDetailModel
 import io.agora.scene.show.service.ShowServiceProtocol
 import io.agora.scene.widget.basic.BindingSingleAdapter
 import io.agora.scene.widget.basic.BindingViewHolder
@@ -16,19 +16,19 @@ import io.agora.scene.widget.utils.StatusBarUtil
 class RoomListActivity : AppCompatActivity() {
 
     private val mBinding by lazy { ShowRoomListActivityBinding.inflate(LayoutInflater.from(this)) }
-    private lateinit var mRoomAdapter: BindingSingleAdapter<ShowRoomListModel, ShowRoomItemBinding>
+    private lateinit var mRoomAdapter: BindingSingleAdapter<ShowRoomDetailModel, ShowRoomItemBinding>
     private val mService by lazy { ShowServiceProtocol.getImplInstance() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        StatusBarUtil.setDarkStatusIcon(window, true)
+        StatusBarUtil.hideStatusBar(window, true)
         setContentView(mBinding.root)
         initView()
     }
 
     private fun initView() {
         mBinding.titleView.setLeftClick { finish() }
-        mRoomAdapter = object : BindingSingleAdapter<ShowRoomListModel, ShowRoomItemBinding>() {
+        mRoomAdapter = object : BindingSingleAdapter<ShowRoomDetailModel, ShowRoomItemBinding>() {
             override fun onBindViewHolder(
                 holder: BindingViewHolder<ShowRoomItemBinding>,
                 position: Int
@@ -49,7 +49,7 @@ class RoomListActivity : AppCompatActivity() {
         mBinding.btnCreateRoom2.setOnClickListener { goLivePrepareActivity() }
     }
 
-    private fun updateList(data: List<ShowRoomListModel>) {
+    private fun updateList(data: List<ShowRoomDetailModel>) {
         mBinding.tvTips1.isVisible = data.isEmpty()
         mBinding.ivBgMobile.isVisible = data.isEmpty()
         mBinding.btnCreateRoom2.isVisible = data.isEmpty()
@@ -60,7 +60,7 @@ class RoomListActivity : AppCompatActivity() {
         mBinding.smartRefreshLayout.finishRefresh()
     }
 
-    private fun updateRoomItem(binding: ShowRoomItemBinding, roomInfo: ShowRoomListModel) {
+    private fun updateRoomItem(binding: ShowRoomItemBinding, roomInfo: ShowRoomDetailModel) {
         binding.tvRoomName.text = roomInfo.roomName
         binding.tvRoomId.text = getString(R.string.show_room_id, roomInfo.roomNo)
         binding.tvUserCount.text = getString(R.string.show_user_count, roomInfo.roomUserCount)
@@ -75,10 +75,9 @@ class RoomListActivity : AppCompatActivity() {
         }
     }
 
-    private fun goLiveDetailActivity(roomInfo: ShowRoomListModel){
-        Intent(this, LiveDetailActivity::class.java).let {
-            it.putExtra("roomInfo", roomInfo)
-            startActivity(it)
-        }
+    private fun goLiveDetailActivity(roomInfo: ShowRoomDetailModel){
+        mService.joinRoom(roomInfo.roomNo, {
+            LiveDetailActivity.launch(this, it)
+        })
     }
 }
--- Android/scenes/show/src/main/java/io/agora/scene/show/service/ShowSyncManagerServiceImpl.kt
+++ Android/scenes/show/src/main/java/io/agora/scene/show/service/ShowSyncManagerServiceImpl.kt
@@ -1,10 +1,12 @@
 package io.agora.scene.show.service
 
 import android.content.Context
+import android.os.Handler
+import android.os.Looper
 import io.agora.scene.base.BuildConfig
-import io.agora.syncmanager.rtm.IObject
-import io.agora.syncmanager.rtm.Sync
-import io.agora.syncmanager.rtm.SyncManagerException
+import io.agora.scene.base.manager.UserManager
+import io.agora.syncmanager.rtm.*
+import kotlin.random.Random
 
 class ShowSyncManagerServiceImpl(
     private val context: Context,
@@ -15,18 +17,31 @@ class ShowSyncManagerServiceImpl(
     @Volatile
     private var syncInitialized = false
 
+
+    // global cache data
+    private val roomMap = mutableMapOf<String, ShowRoomDetailModel>()
+
+    // current room cache data
+    private var currRoomNo : String = ""
+    private var currSceneReference: SceneReference? = null
+
     override fun getRoomList(
-        success: (List<ShowRoomListModel>) -> Unit,
+        success: (List<ShowRoomDetailModel>) -> Unit,
         error: ((Exception) -> Unit)?
     ) {
         initSync {
-            Sync.Instance().getScenes(object : Sync.DataListCallback{
+            Sync.Instance().getScenes(object : Sync.DataListCallback {
                 override fun onSuccess(result: MutableList<IObject>?) {
-                    val roomList = result!!.map { it.toObject(ShowRoomListModel::class.java).apply { objectId = it.id } }
+                    val roomList = result!!.map {
+                        it.toObject(ShowRoomDetailModel::class.java)
+                    }
+                    roomMap.clear()
+                    roomList.forEach { roomMap[it.roomNo] = it.copy() }
                     success.invoke(roomList.sortedBy { it.crateAt })
                 }
 
                 override fun onFail(exception: SyncManagerException?) {
+                    errorHandler.invoke(exception!!)
                     error?.invoke(exception!!)
                 }
             })
@@ -34,23 +49,90 @@ class ShowSyncManagerServiceImpl(
     }
 
     override fun createRoom(
-        room: ShowRoomListModel,
+        roomName: String,
         success: (ShowRoomDetailModel) -> Unit,
         error: ((Exception) -> Unit)?
     ) {
-        TODO("Not yet implemented")
+        initSync {
+            val roomDetail = ShowRoomDetailModel(
+                (Random(System.currentTimeMillis()).nextInt(10000) + 100000).toString(),
+                roomName,
+                0,
+                "",
+                UserManager.getInstance().user.userNo,
+                ShowRoomStatus.activity.value,
+                System.currentTimeMillis().toDouble(),
+                System.currentTimeMillis().toDouble()
+            )
+            val scene = Scene().apply {
+                id = roomDetail.roomNo
+                userId = roomDetail.ownerId
+                property = roomDetail.toMap()
+            }
+            Sync.Instance().createScene(
+                scene,
+                object : Sync.Callback {
+                    override fun onSuccess() {
+                        roomMap[roomDetail.roomNo] = roomDetail.copy()
+                        success.invoke(roomDetail)
+                    }
+
+                    override fun onFail(exception: SyncManagerException?) {
+                        errorHandler.invoke(exception!!)
+                        error?.invoke(exception!!)
+                    }
+                })
+        }
     }
 
     override fun joinRoom(
-        room: ShowRoomListModel,
+        roomNo: String,
         success: (ShowRoomDetailModel) -> Unit,
         error: ((Exception) -> Unit)?
     ) {
-        TODO("Not yet implemented")
+        if(currRoomNo.isNotEmpty()){
+            error?.invoke(RuntimeException("There is a room joined or joining now!"))
+            return
+        }
+        currRoomNo = roomNo
+        initSync {
+            Sync.Instance().joinScene(
+                roomNo, object: Sync.JoinSceneCallback{
+                    override fun onSuccess(sceneReference: SceneReference?) {
+
+                        this@ShowSyncManagerServiceImpl.currSceneReference = sceneReference!!
+                        success.invoke(roomMap[roomNo]!!)
+                    }
+
+                    override fun onFail(exception: SyncManagerException?) {
+                        errorHandler.invoke(exception!!)
+                        error?.invoke(exception!!)
+                        currRoomNo = ""
+                    }
+                }
+            )
+        }
     }
 
-    override fun leaveRoom(success: (() -> Unit)?, error: ((Exception) -> Unit)?) {
-        TODO("Not yet implemented")
+    override fun leaveRoom() {
+        if(currRoomNo.isEmpty()){
+            return
+        }
+        val roomDetail = roomMap[currRoomNo] ?: return
+        if(roomDetail.ownerId == UserManager.getInstance().user.userNo){
+            val roomNo = currRoomNo
+            currSceneReference?.delete(object:Sync.Callback{
+                override fun onSuccess() {
+                    roomMap.remove(roomNo)
+                }
+
+                override fun onFail(exception: SyncManagerException?) {
+                    errorHandler.invoke(exception!!)
+                }
+            })
+        }
+        currRoomNo = ""
+        currSceneReference = null
     }
 
     override fun getAllUserList(success: (List<ShowUser>) -> Unit, error: ((Exception) -> Unit)?) {
@@ -161,9 +243,9 @@ class ShowSyncManagerServiceImpl(
         Sync.Instance().init(
             context,
             mutableMapOf(Pair("appid", BuildConfig.AGORA_APP_ID), Pair("defaultChannel", kSceneId)),
-            object : Sync.Callback{
+            object : Sync.Callback {
                 override fun onSuccess() {
-
+                    Handler(Looper.getMainLooper()).post { complete.invoke() }
                 }
 
                 override fun onFail(exception: SyncManagerException?) {
