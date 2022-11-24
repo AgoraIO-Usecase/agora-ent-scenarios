d8c33bd117d26e48322f524a9a99725aceeea4f7
wushengtao <tamworth@163.com>
2022-11-24 18:04:22 +0800

    feat: udpate service

--- iOS/AgoraEntScenarios/Scenes/Show/Service/ShowServiceModel.swift
+++ iOS/AgoraEntScenarios/Scenes/Show/Service/ShowServiceModel.swift
@@ -9,19 +9,28 @@ import Foundation
 
 
 enum ShowRoomStatus: Int {
-    case activity = 0    //直播中
-    case end = 1         //直播结束
+    /// 直播中
+    case activity = 0
+    /// 直播结束
+    case end = 1
 }
 
 enum ShowRoomRequestStatus: Int {
     /// 等待中
     case waitting = 1
-    ///  已接受
-    case accept = 2
+    /// 已接受
+    case accepted = 2
     /// 已拒绝
-    case refuse = 3
-    /// 已结束
-    case end = 4
+    case rejected = 3
+}
+
+enum ShowInteractionStatus: Int {
+    /// 空闲
+    case idle = 0
+    /// 连麦中
+    case onSeat = 1
+    /// pk中
+    case pking = 2
 }
 
 @objcMembers
@@ -32,16 +41,17 @@ class ShowBaseInfo: NSObject {
 /// 房间列表信息
 @objcMembers
 class ShowRoomListModel: ShowBaseInfo {
-    var roomNo: String?                           //房间号
-    var roomName: String?                         //房间名
-    var roomUserCount: Int = 1                    //房间人数
-    var thumbnailId: String?                      //缩略图id
-    var ownerId: String?                          //房主user id
-    var ownerAvater: String?                      //房主头像
-    var ownerName: String?                        //房主名
-    var roomStatus: ShowRoomStatus = .activity    //直播状态
-    var createdAt: Int64 = 0                      //创建时间，与19700101时间比较的毫秒数
-    var updatedAt: Int64 = 0                      //更新时间
+    var roomId: String?                                //房间号
+    var roomName: String?                              //房间名
+    var roomUserCount: Int = 1                         //房间人数
+    var thumbnailId: String?                           //缩略图id
+    var ownerId: String?                               //房主user id (rtc uid)
+    var ownerAvater: String?                           //房主头像
+    var ownerName: String?                             //房主名
+    var roomStatus: ShowRoomStatus = .activity         //直播状态
+    var interactStatus: ShowInteractionStatus = .idle  //互动状态
+    var createdAt: Int64 = 0                           //创建时间，与19700101时间比较的毫秒数
+    var updatedAt: Int64 = 0                           //更新时间
 }
 
 
@@ -53,7 +63,7 @@ class ShowRoomDetailModel: ShowRoomListModel {
 ///用户信息
 @objcMembers
 class ShowUser: ShowBaseInfo {
-    var userId: String?              //用户id
+    var userId: String?              //用户id (rtc uid)
     var avatar: String?              //用户头像
     var userName: String?            //用户名
 }
@@ -61,7 +71,7 @@ class ShowUser: ShowBaseInfo {
 /// 聊天消息
 @objcMembers
 class ShowMessage: ShowBaseInfo {
-    var userId: String?        //用户id
+    var userId: String?        //用户id (rtc uid)
     var userName: String?      //用户名
     var message: String?       //用户名
     var createAt: Int64 = 0    //创建时间，与19700101时间比较的毫秒数
@@ -69,7 +79,7 @@ class ShowMessage: ShowBaseInfo {
 
 /// 连麦申请
 class ShowMicSeatApply: ShowBaseInfo {
-    var userId: String?                              //用户id
+    var userId: String?                              //用户id (rtc uid)
     var userAvatar: String?                          //用户头像
     var userName: String?                            //用户名
     var status: ShowRoomRequestStatus = .waitting    //申请状态
@@ -79,7 +89,7 @@ class ShowMicSeatApply: ShowBaseInfo {
 
 /// 连麦邀请
 class ShowMicSeatInvitation: ShowBaseInfo {
-    var userId: String?                              //用户id
+    var userId: String?                              //用户id (rtc uid)
     var userAvatar: String?                          //用户头像
     var userName: String?                            //用户名
     var fromUserId: String?                          //发起邀请用户id
@@ -89,11 +99,20 @@ class ShowMicSeatInvitation: ShowBaseInfo {
 
 /// PK邀请
 class ShowPKInvitation: ShowBaseInfo {
-    var userId: String?                              //被pk用户id
+    var userId: String?                              //被pk用户id (rtc uid)
     var roomId: String?                              //被pk房间id
-    var fromUserId: String?                          //发起Pk用户id
+    var fromUserId: String?                          //发起Pk用户id (rtc uid)
     var fromName: String?                            //发起Pk用户名
     var fromRoomId: String?                          //发起Pk房间id
     var status: ShowRoomRequestStatus = .waitting    //邀请状态
     var createdAt: Int64 = 0                         //创建时间，与19700101时间比较的毫秒数
+}
+
+//连麦/Pk模型
+class ShowInteractionInfo: ShowBaseInfo {
+    var userId: String?                                 //用户id (rtc uid)
+    var roomId: String?                                 //用户所在房间id
+    var status: ShowRoomRequestStatus = .waitting       //状态
+    var interactStatus: ShowInteractionStatus = .idle   //交互类型
+    var createdAt: Int64 = 0                            //创建时间，与19700101时间比较的毫秒数
 }
