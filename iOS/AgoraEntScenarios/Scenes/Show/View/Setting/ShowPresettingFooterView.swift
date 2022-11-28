31dcac9c13c5fbecb4e1b5a3b72caeefbbdd9e90
yiwangwuqianfjp <fan_jun_peng@163.com>
2022-11-28 18:14:08 +0800

    fix bug

---
+++ iOS/AgoraEntScenarios/Scenes/Show/View/Setting/ShowPresettingFooterView.swift
@@ -0,0 +1,20 @@
+//
+//  ShowPresettingFooterView.swift
+//  AgoraEntScenarios
+//
+//  Created by FanPengpeng on 2022/11/28.
+//
+
+import UIKit
+
+class ShowPresettingFooterView: UITableViewHeaderFooterView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
