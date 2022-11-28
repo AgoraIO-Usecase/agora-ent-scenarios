f688e54f399bd9f76a7e48e5409cec19e9bc8841
chenpark <1120627616@qq.com>
2022-11-28 11:14:10 +0800

    update mic service

--- iOS/AgoraEntScenarios/Scenes/VoiceChatRoom/Views/VoiceChat/AgoraChatRoomNormalRtcView.swift
+++ iOS/AgoraEntScenarios/Scenes/VoiceChatRoom/Views/VoiceChat/AgoraChatRoomNormalRtcView.swift
@@ -129,7 +129,7 @@ extension AgoraChatRoomNormalRtcView: UICollectionViewDelegate, UICollectionView
             /*
              0: 正常 1: 闭麦 2: 禁言 3: 锁麦 4: 锁麦和禁言 -1: 空闲
              */
-            if let mic_info = micInfos?[indexPath.item] {
+            if let mic_info = micInfos?[safe:indexPath.row] {
                 cell.refreshUser(with: mic_info)
             } else {
                 cell.cellType = .AgoraChatRoomBaseUserCellTypeAdd
@@ -137,7 +137,7 @@ extension AgoraChatRoomNormalRtcView: UICollectionViewDelegate, UICollectionView
             return cell
         } else {
             let cell: AgoraChatRoomBaseAlienCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: aIdentifier, for: indexPath) as! AgoraChatRoomBaseAlienCollectionViewCell
-            if let mic_info = micInfos?[indexPath.row] {
+            if let mic_info = micInfos?[safe:indexPath.row] {
                 cell.cellType = mic_info.status == 5 ? .AgoraChatRoomBaseUserCellTypeActived : .AgoraChatRoomBaseUserCellTypeNonActived
             }
 
