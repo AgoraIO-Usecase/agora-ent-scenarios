bbc9ead66784fd23cc5797fd966469a40f51ec13
apex-wang <1746807718@qq.com>
2022-11-28 20:59:51 +0800

    update sendSystemMsg data info

--- Android/scenes/voice/voice/src/main/java/io/agora/scene/voice/ui/widget/primary/MenuItemClickListener.java
+++ Android/scenes/voice/voice/src/main/java/io/agora/scene/voice/ui/widget/primary/MenuItemClickListener.java
@@ -4,8 +4,6 @@ import android.view.View;
 
 public interface MenuItemClickListener {
     void onChatExtendMenuItemClick(int itemId, View view);
-    void onInputViewFocusChange(boolean focus);
     void onInputLayoutClick();
-    void onEmojiClick(boolean isShow);
     void onSendMessage(String content);
 }
