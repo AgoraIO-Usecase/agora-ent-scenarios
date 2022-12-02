d252c7607b74c1ab73a107109c01e950e4baa809
CYG <cyg@CYGdeMacBook-Pro-2.local>
2022-12-02 16:01:38 +0800

    optimize resource name for release

--- Android/scenes/ktv/src/main/java/io/agora/scene/ktv/widget/LrcControlView.java
+++ Android/scenes/ktv/src/main/java/io/agora/scene/ktv/widget/LrcControlView.java
@@ -217,11 +217,13 @@ public class LrcControlView extends FrameLayout implements View.OnClickListener 
         if (songPlaying != null && songPlaying.isChorus()) {
             if (UserManager.getInstance().getUser().userNo.equals(songPlaying.getUserNo())) {
                 mBinding.scoreControlView.setVisibility(VISIBLE);
+                mBinding.scoreControlView.setText(getContext().getString(R.string.ktv_score_formatter, 0.00));
             } else {
                 mBinding.scoreControlView.setVisibility(GONE);
             }
         } else if (songPlaying != null && !songPlaying.isChorus()) {
             mBinding.scoreControlView.setVisibility(VISIBLE);
+            mBinding.scoreControlView.setText(getContext().getString(R.string.ktv_score_formatter, 0.00));
         } else {
             mBinding.scoreControlView.setVisibility(GONE);
         }
@@ -303,7 +305,7 @@ public class LrcControlView extends FrameLayout implements View.OnClickListener 
     }
 
     public void updateScore(double score) {
-        mBinding.scoreControlView.setText(getContext().getString(R.string.score_formatter, score));
+        mBinding.scoreControlView.setText(getContext().getString(R.string.ktv_score_formatter, score));
     }
 
     @Override
