9520f3b2eb63f195d3beca9a4041de7e51977ca6
CYG <cyg@CYGdeMacBook-Pro-2.local>
2022-12-02 16:01:38 +0800

    optimize resource name for release

--- Android/app/src/main/java/com/agora/entfulldemo/webview/WebViewActivity.java
+++ Android/app/src/main/java/com/agora/entfulldemo/webview/WebViewActivity.java
@@ -7,7 +7,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.agora.entfulldemo.R;
-import com.agora.entfulldemo.databinding.ActivityWebviewBinding;
+import com.agora.entfulldemo.databinding.AppActivityWebviewBinding;
 import com.alibaba.android.arouter.facade.annotation.Autowired;
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.alibaba.android.arouter.launcher.ARouter;
@@ -18,7 +18,7 @@ import io.agora.scene.base.component.BaseViewBindingActivity;
 import kotlin.jvm.JvmField;
 
 @Route(path = PagePathConstant.pageWebView)
-public class WebViewActivity extends BaseViewBindingActivity<ActivityWebviewBinding> {
+public class WebViewActivity extends BaseViewBindingActivity<AppActivityWebviewBinding> {
     /**
      * h5地址
      */
@@ -28,19 +28,19 @@ public class WebViewActivity extends BaseViewBindingActivity<ActivityWebviewBind
 
 
     @Override
-    protected ActivityWebviewBinding getViewBinding(@NonNull LayoutInflater layoutInflater) {
-        return ActivityWebviewBinding.inflate(layoutInflater);
+    protected AppActivityWebviewBinding getViewBinding(@NonNull LayoutInflater layoutInflater) {
+        return AppActivityWebviewBinding.inflate(layoutInflater);
     }
 
     @Override
     public void initView(@Nullable Bundle savedInstanceState) {
         ARouter.getInstance().inject(this);
         if (url.contains("user_agreement")) {
-            getBinding().titleView.setTitle(getString(R.string.user_agreement));
+            getBinding().titleView.setTitle(getString(R.string.app_user_agreement));
         } else if (url.contains("about-us")) {
-            getBinding().titleView.setTitle(getString(R.string.about_us));
+            getBinding().titleView.setTitle(getString(R.string.app_about_us));
         } else if (url.contains("privacy_policy")) {
-            getBinding().titleView.setTitle(getString(R.string.privacy_agreement));
+            getBinding().titleView.setTitle(getString(R.string.app_privacy_agreement));
         }
         getBinding().webView.loadUrl(url);
     }
