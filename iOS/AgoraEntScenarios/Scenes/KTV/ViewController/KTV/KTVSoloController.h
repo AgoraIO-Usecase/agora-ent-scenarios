42ea0fe81e2c73ab4c14d38e6f71bf105fe5eba4
zhangqianze <zhangqianze@agora.io>
2022-12-04 01:55:59 +0800

    fix: fix sync

--- iOS/AgoraEntScenarios/Scenes/KTV/ViewController/KTV/KTVSoloController.h
+++ iOS/AgoraEntScenarios/Scenes/KTV/ViewController/KTV/KTVSoloController.h
@@ -9,11 +9,15 @@
 @import AgoraRtcKit;
 
 NS_ASSUME_NONNULL_BEGIN
+typedef void (^sendStreamSuccess)(BOOL ifSuccess);
 typedef enum : NSUInteger {
     KTVSingRoleMainSinger,
     KTVSingRoleAudience
 } KTVSingRole;
-
+typedef enum : NSUInteger {
+    KTVPlayerTrackOrigin = 0,
+    KTVPlayerTrackAcc = 1
+} KTVPlayerTrackMode;
 typedef enum : NSUInteger {
     KTVLoadSongStateOK,
     KTVLoadSongStateInProgress,
@@ -25,6 +29,7 @@ typedef enum : NSUInteger {
 @protocol KTVSoloControllerDelegate <NSObject>
 
 - (void)controller:(KTVSoloController*)controller song:(NSInteger)songCode didChangedToState:(AgoraMediaPlayerState)state;
+- (void)controller:(KTVSoloController*)controller song:(NSInteger)songCode didChangedToPosition:(NSInteger)position;
 
 @end
 
@@ -32,12 +37,15 @@ typedef enum : NSUInteger {
 
 @property(nonatomic, weak)id<KTVSoloControllerDelegate> delegate;
 
--(id)initWithRtcEngine:(AgoraRtcEngineKit *)engine musicCenter:(AgoraMusicContentCenter*)musicCenter player:(nonnull id<AgoraMusicPlayerProtocol>)rtcMediaPlayer delegate:(id<KTVSoloControllerDelegate>)delegate;
+-(id)initWithRtcEngine:(AgoraRtcEngineKit *)engine musicCenter:(AgoraMusicContentCenter*)musicCenter player:(nonnull id<AgoraMusicPlayerProtocol>)rtcMediaPlayer dataStreamId:(NSInteger)streamId delegate:(id<KTVSoloControllerDelegate>)delegate;
 -(void)loadSong:(NSInteger)songCode asRole:(KTVSingRole)role withCallback:(void (^ _Nullable)(NSInteger songCode, NSString* lyricUrl, KTVSingRole role, KTVLoadSongState state))block;
 -(void)playSong:(NSInteger)songCode asRole:(KTVSingRole)role;
 -(void)stopSong;
 -(void)resumePlay;
 -(void)pausePlay;
+-(void)selectTrackMode:(KTVPlayerTrackMode)mode;
+-(void)sendStreamMessageWithDict:(NSDictionary *)dict
+                         success:(_Nullable sendStreamSuccess)success;
 @end
 
 NS_ASSUME_NONNULL_END
