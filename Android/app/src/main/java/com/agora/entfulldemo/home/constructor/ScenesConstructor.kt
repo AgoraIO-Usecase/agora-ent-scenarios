e7f1f78e2bc53a824db24ee97e83a226843558f1
xucz <xgfd3@users.noreply.github.com>
2022-12-08 23:39:25 +0800

    [Android]fix cherry-pick problem.

--- Android/app/src/main/java/com/agora/entfulldemo/home/constructor/ScenesConstructor.kt
+++ Android/app/src/main/java/com/agora/entfulldemo/home/constructor/ScenesConstructor.kt
@@ -21,35 +21,35 @@ object ScenesConstructor {
             ),
             ScenesModel(
                 "io.agora.scene.voice.ui.activity.VoiceRoomListActivity",
-                context.getString(R.string.voice_chat),
+                context.getString(R.string.app_voice_chat),
                 R.mipmap.bg_btn_home1,
                 R.mipmap.bg_btn_home_ktv,
                 true
             ),
             ScenesModel(
                 "io.agora.scene.show.RoomListActivity",
-                context.getString(R.string.show_live),
+                context.getString(R.string.app_show_live),
                 R.mipmap.bg_btn_home1,
                 R.mipmap.bg_btn_home_ktv,
                 true
             ),
             ScenesModel(
                 "",
-                context.getString(R.string.meta_live),
+                context.getString(R.string.app_meta_live),
                 R.mipmap.bg_btn_home2,
                 R.mipmap.bg_btn_home_live,
                 false
             ),
             ScenesModel(
                 "",
-                context.getString(R.string.meta_chatting),
+                context.getString(R.string.app_meta_chatting),
                 R.mipmap.bg_btn_home3,
                 R.mipmap.bg_btn_home_chat,
                 false
             ),
             ScenesModel(
                 "",
-                context.getString(R.string.interactive_games),
+                context.getString(R.string.app_interactive_games),
                 R.mipmap.bg_btn_home4,
                 R.mipmap.bg_btn_home_youxi,
                 false
