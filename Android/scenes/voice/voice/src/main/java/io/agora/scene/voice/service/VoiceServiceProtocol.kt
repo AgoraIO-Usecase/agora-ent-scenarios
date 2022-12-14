6a1abdd58a2ce372bcc6539a05e55ddccdd358fa
xucz <xgfd3@users.noreply.github.com>
2022-12-14 14:16:33 +0800

    [Voice][Android]fix problems of jira (FT-8/FT-36/FT-40).

--- Android/scenes/voice/voice/src/main/java/io/agora/scene/voice/service/VoiceServiceProtocol.kt
+++ Android/scenes/voice/voice/src/main/java/io/agora/scene/voice/service/VoiceServiceProtocol.kt
@@ -17,6 +17,7 @@ interface VoiceServiceProtocol {
         const val ERR_FAILED = 1
         const val ERR_LOGIN_ERROR = 2
         const val ERR_LOGIN_SUCCESS = 3
+        const val ERR_ROOM_UNAVAILABLE = 4
         private val instance by lazy {
             // VoiceChatServiceImp()
             VoiceSyncManagerServiceImp(VoiceBuddyFactory.get().getVoiceBuddy().application()) { error ->
