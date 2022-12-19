787b832af7d886d0e02584d91a65f3421702a950
wushengtao <tamworth@163.com>
2022-12-19 17:48:57 +0800

    feat: remove ChameleonFramework & CYLTabBarController

--- iOS/AgoraEntScenarios/HomeMenu/Main/VLMainRootViewController.h
+++ iOS/AgoraEntScenarios/HomeMenu/Main/VLMainRootViewController.h
@@ -4,7 +4,7 @@
 //
 
 #import "KTVMacro.h"
-#import "CYLTabBarController.h"
+//#import "CYLTabBarController.h"
 NS_ASSUME_NONNULL_BEGIN
 
 //@interface VLMainRootViewController : UINavigationController
