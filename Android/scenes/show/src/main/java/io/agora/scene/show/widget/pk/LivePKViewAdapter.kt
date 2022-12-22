e65093ce432eb909cd931fa85fc8151d1961601b
CYG <cyg@CYGdeMacBook-Pro-2.local>
2022-12-22 15:08:39 +0800

    fix spell error in RoomDetail and fix pk bugs

--- Android/scenes/show/src/main/java/io/agora/scene/show/widget/pk/LivePKViewAdapter.kt
+++ Android/scenes/show/src/main/java/io/agora/scene/show/widget/pk/LivePKViewAdapter.kt
@@ -5,31 +5,36 @@ import io.agora.scene.base.GlideApp
 import io.agora.scene.show.R
 import io.agora.scene.show.databinding.ShowLivePkRequestMessageBinding
 import io.agora.scene.show.service.ShowInteractionStatus
-import io.agora.scene.show.service.ShowRoomDetailModel
 import io.agora.scene.widget.basic.BindingSingleAdapter
 import io.agora.scene.widget.basic.BindingViewHolder
 import io.agora.scene.widget.utils.CenterCropRoundCornerTransform
 
-class LivePKViewAdapter: BindingSingleAdapter<ShowRoomDetailModel, ShowLivePkRequestMessageBinding>() {
+class LivePKViewAdapter: BindingSingleAdapter<LiveRoomConfig, ShowLivePkRequestMessageBinding>() {
     override fun onBindViewHolder(
         holder: BindingViewHolder<ShowLivePkRequestMessageBinding>,
         position: Int
     ) {
         val roomItem = getItem(position)!!
         val binding = holder.binding
-        binding.titleItemBoardcasterStatus.text = roomItem.ownerName
+        binding.titleItemBoardcasterStatus.text = roomItem.getOwnerName()
         binding.coverBoardcasterIcon.visibility = View.VISIBLE
-        GlideApp.with(binding.coverBoardcasterIcon).load(roomItem.ownerAvater)
+        GlideApp.with(binding.coverBoardcasterIcon).load(roomItem.getOwnerAvatar())
             .fallback(R.mipmap.show_default_icon)
             .error(R.mipmap.show_default_icon)
             .transform(CenterCropRoundCornerTransform(10))
             .into(binding.coverBoardcasterIcon)
-        when (roomItem.interactStatus) {
+        when (roomItem.getInteractStatus()) {
             ShowInteractionStatus.idle.value -> {
-                binding.btnItemRequest.isEnabled = true
-                binding.btnItemRequest.setText(R.string.show_application)
-                binding.btnItemRequest.setOnClickListener {
-                    onClickListener.onClick(roomItem, position)
+                if (roomItem.isWaitingForPK()) {
+                    binding.btnItemRequest.isEnabled = false
+                    binding.btnItemRequest.setText(R.string.show_application_waitting)
+                    binding.btnItemRequest.setOnClickListener(null)
+                } else {
+                    binding.btnItemRequest.isEnabled = true
+                    binding.btnItemRequest.setText(R.string.show_application)
+                    binding.btnItemRequest.setOnClickListener {
+                        onClickListener.onClick(roomItem, position)
+                    }
                 }
             }
             ShowInteractionStatus.pking.value -> {
@@ -47,7 +52,7 @@ class LivePKViewAdapter: BindingSingleAdapter<ShowRoomDetailModel, ShowLivePkReq
 
     private lateinit var onClickListener : OnClickListener
     interface OnClickListener {
-        fun onClick(roomItem: ShowRoomDetailModel, position: Int)
+        fun onClick(roomItem: LiveRoomConfig, position: Int)
     }
     fun setClickListener(listener : OnClickListener) {
         onClickListener = listener
