112338296dcb49d077b17058a8d7cbcb5da733d6
xucz <xgfd3@users.noreply.github.com>
2022-12-23 21:23:35 +0800

    [Show][Android]remove bytedance beauty and update README.

--- Android/scenes/show/src/main/java/com/bytedance/labcv/core/Config.java
+++
@@ -1,9 +0,0 @@
-package com.bytedance.labcv.core;
-
-/**
- * Created on 2021/7/2 10:13
- */
-public class Config {
-    public static final String LICENSE_NAME = "agora_test_20220805_20230208_io.agora.entfull_4.2.3.licbag";
-    public static final String CHECK_RESULT_BROADCAST_ACTION = "com.bytedance.labcv.core.check_result:action";
-}
