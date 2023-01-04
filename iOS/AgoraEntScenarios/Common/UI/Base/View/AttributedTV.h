90a1c3e490c6cd86bfff72788f680d75f1f4ea38
chenpark <1120627616@qq.com>
2023-01-04 18:03:41 +0800

    remove mjrefresh and yytext

---
+++ iOS/AgoraEntScenarios/Common/UI/Base/View/AttributedTV.h
@@ -0,0 +1,16 @@
+//
+//  AttributedTV.h
+//  text
+//
+//  Created by CP on 2023/1/4.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AttributedTV : UITextView
+-(instancetype)initWithFrame:(CGRect)frame text:(NSString *)text AttributedStringS:(NSArray *)strings ranges:(NSArray *)ranges textColor:(UIColor *)textColor attributeTextColor:(UIColor *)attributeTextColor;
+@end
+
+NS_ASSUME_NONNULL_END
