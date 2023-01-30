bb50f8aa89ce5d2fab15dfe658f10e0716c91cb8
xucz <xgfd3@users.noreply.github.com>
2023-01-30 17:18:25 +0800

    [Show][Android]perfect video view render and fix bugs.

--- Android/scenes/show/src/main/java/io/agora/scene/show/VideoSwitcher.kt
+++ Android/scenes/show/src/main/java/io/agora/scene/show/VideoSwitcher.kt
@@ -1,12 +1,18 @@
 package io.agora.scene.show
 
+import android.view.ViewGroup
+import androidx.lifecycle.LifecycleOwner
 import io.agora.rtc2.ChannelMediaOptions
+import io.agora.rtc2.Constants
 import io.agora.rtc2.IRtcEngineEventHandler
 import io.agora.rtc2.RtcConnection
 
+/**
+ *
+ */
 interface VideoSwitcher {
 
-    class IChannelEventListener(
+    data class IChannelEventListener(
         var onTokenGenerateFailedException: ((error: Throwable)->Unit)? = null,
         var onChannelJoined: ((connection: RtcConnection)->Unit)? = null,
         var onUserJoined: ((uid: Int) -> Unit)? = null,
@@ -21,6 +27,14 @@ interface VideoSwitcher {
         var onUplinkNetworkInfoUpdated: ((info: IRtcEngineEventHandler.UplinkNetworkInfo) -> Unit)? = null,
         var onDownlinkNetworkInfoUpdated: ((info: IRtcEngineEventHandler.DownlinkNetworkInfo) -> Unit)? = null,
         var onContentInspectResult: ((result: Int) -> Unit)? = null,
+    )
+
+    data class VideoCanvasContainer(
+        val lifecycleOwner: LifecycleOwner,
+        val container: ViewGroup,
+        val uid: Int,
+        val viewIndex: Int = 0,
+        val renderMode: Int = Constants.RENDER_MODE_HIDDEN,
     )
 
     /**
@@ -51,5 +65,15 @@ interface VideoSwitcher {
      * 离开频道，如果在已预加载的频道则只取消订阅音视频流
      */
     fun leaveChannel(connection: RtcConnection): Boolean
+
+    /**
+     * 渲染远端视频，相比于RtcEngineEx.setupRemoteVideoEx，这里会缓存渲染视图，减少渲染时不断重复创建渲染视图，提高渲染速度
+     */
+    fun setupRemoteVideo(connection: RtcConnection, container: VideoCanvasContainer)
+
+    /**
+     * 渲染本地视频，相比于RtcEngineEx.setupLocalVideo，这里会缓存渲染视图，减少渲染时不断重复创建渲染视图，提高渲染速度
+     */
+    fun setupLocalVideo(container: VideoCanvasContainer)
 
 }
