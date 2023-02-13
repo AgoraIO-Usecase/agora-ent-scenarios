6fad93b52272c8ebc52294c8b9753510bddaa443
wushengtao <tamworth@163.com>
2023-02-13 11:49:20 +0800

    fix: mcc song result count fixed

--- iOS/AgoraEntScenarios/Scenes/KTV/View/KTV/View/KTVSongGallery/VLSearchSongResultView.m
+++ iOS/AgoraEntScenarios/Scenes/KTV/View/KTV/View/KTVSongGallery/VLSearchSongResultView.m
@@ -97,7 +97,7 @@ AgoraMusicContentCenterEventDelegate
     self.requestId =
     [[AppContext shared].agoraMcc searchMusicWithKeyWord:keyWord ? keyWord : @""
                                                     page:self.page
-                                                pageSize:100
+                                                pageSize:50
                                               jsonOption:nil];
 }
 
