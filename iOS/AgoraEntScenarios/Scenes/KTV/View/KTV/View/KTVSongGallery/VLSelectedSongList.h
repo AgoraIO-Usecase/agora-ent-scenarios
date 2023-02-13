1fb5753ff49c5210e43219a225041b069c3439f8
wushengtao <tamworth@163.com>
2023-02-13 17:56:25 +0800

    fix: sync button status of choose song list

--- iOS/AgoraEntScenarios/Scenes/KTV/View/KTV/View/KTVSongGallery/VLSelectedSongList.h
+++ iOS/AgoraEntScenarios/Scenes/KTV/View/KTV/View/KTVSongGallery/VLSelectedSongList.h
@@ -15,6 +15,7 @@ NS_ASSUME_NONNULL_BEGIN
 @end
 
 @interface VLSelectedSongList : UIView
+@property (nonatomic, strong) NSArray *selSongsArray;
 
 - (instancetype)initWithFrame:(CGRect)frame withDelegate:(id<VLSelectedSongListDelegate>)delegate withRoomNo:(NSString *)roomNo ifChorus:(BOOL)ifChorus;
 
