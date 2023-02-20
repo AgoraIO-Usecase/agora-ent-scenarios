cf61ec32ded86fa2d2537f1b2f25cb76a2d6a2ce
zhaoyongqiang <zhaoyongqiang@agora.io>
2023-02-20 17:22:30 +0800

    添加全局debug模式设置

--- iOS/AgoraEntScenarios/HomeMenu/Mine/View/VLMineView.h
+++ iOS/AgoraEntScenarios/HomeMenu/Mine/View/VLMineView.h
@@ -14,6 +14,7 @@ typedef enum : NSUInteger {
     VLMineViewClickTypeAboutUS,            // 关于我们
     VLMineViewClickTypeLogout,             // 退出登录
     VLMineViewClickTypeDestroyAccount,     // 注销账号
+    VLMineViewClickTypeDebug,              // 开发者模式
 } VLMineViewClickType;
 
 typedef enum : NSUInteger {
@@ -38,6 +39,7 @@ typedef enum : NSUInteger {
 - (void)refreseUserInfo:(VLLoginModel *)loginModel;
 - (void)refreseAvatar:(UIImage *)avatar;
 - (void)refreseNickName:(NSString *)nickName;
+- (void)refreshTableView;
 
 @end
 
