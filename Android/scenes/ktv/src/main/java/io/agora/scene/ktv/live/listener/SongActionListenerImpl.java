28922df681cffbf1b43c99c49304b17689d982b2
leo_msi <742831652@qq.com>
2023-02-28 14:28:50 +0800

    fix:逻辑代码迁移到Fragment

--- Android/scenes/ktv/src/main/java/io/agora/scene/ktv/live/listener/SongActionListenerImpl.java
+++ Android/scenes/ktv/src/main/java/io/agora/scene/ktv/live/listener/SongActionListenerImpl.java
@@ -2,6 +2,7 @@ package io.agora.scene.ktv.live.listener;
 
 import android.content.Context;
 import android.text.TextUtils;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -42,27 +43,24 @@ public class SongActionListenerImpl implements OnSongActionListener {
     public void onChooseSongRefreshing(@NonNull SongDialog dialog, int index) {
         // 点歌-列表刷新
         mCurrPage = 1;
-        LiveDataUtils.observerThenRemove(mLifecycleOwner,
-                mViewModel.getSongList(getSongType(index), mCurrPage),
-                list -> {
-                    if (dialog.isVisible()) {
-                        dialog.setChooseRefreshingResult(transSongModel(list));
-                    }
-                });
+        int songType = getSongType(index);
+        Log.e("liu0228", "index = " + index + "    songType = " + songType);
+        LiveDataUtils.observerThenRemove(mLifecycleOwner, mViewModel.getSongList(songType, mCurrPage), list -> {
+            if (dialog.isVisible()) {
+                dialog.setChooseRefreshingResult(transSongModel(list), index);
+            }
+        });
     }
 
     @Override
     public void onChooseSongLoadMore(@NonNull SongDialog dialog, int index) {
         // 点歌-列表加载更多
         mCurrPage++;
-        LiveDataUtils.observerThenRemove(mLifecycleOwner,
-                mViewModel.getSongList(getSongType(index), mCurrPage),
-                list -> {
-                    if (dialog.isVisible()) {
-                        dialog.setChooseLoadMoreResult(transSongModel(list),
-                                list.size() > 0);
-                    }
-                });
+        LiveDataUtils.observerThenRemove(mLifecycleOwner, mViewModel.getSongList(getSongType(index), mCurrPage), list -> {
+            if (dialog.isVisible()) {
+                dialog.setChooseLoadMoreResult(transSongModel(list), list.size() > 0, index);
+            }
+        });
     }
 
     @Override
@@ -81,13 +79,11 @@ public class SongActionListenerImpl implements OnSongActionListener {
     public void onChooseSongChosen(@NonNull SongDialog dialog, @NonNull SongItem songItem) {
         // 点歌
         RoomSelSongModel songModel = songItem.getTag(RoomSelSongModel.class);
-        LiveDataUtils.observerThenRemove(mLifecycleOwner,
-                mViewModel.chooseSong(songModel, isChorus),
-                success -> {
-                    if (success && dialog.isVisible()) {
-                        dialog.setChooseSongItemStatus(songItem, true);
-                    }
-                });
+        LiveDataUtils.observerThenRemove(mLifecycleOwner, mViewModel.chooseSong(songModel, isChorus), success -> {
+            if (success && dialog.isVisible()) {
+                dialog.setChooseSongItemStatus(songItem, true);
+            }
+        });
     }
 
     @Override
@@ -110,6 +106,14 @@ public class SongActionListenerImpl implements OnSongActionListener {
             titles.add(entry.getValue());
         }
         return titles;
+    }
+
+    public List<Integer> getSongTypeList() {
+        List<Integer> list = new ArrayList<>();
+        for (Map.Entry<Integer, String> entry : songTypeMap.entrySet()) {
+            list.add(entry.getKey());
+        }
+        return list;
     }
 
     private int getSongType(int index) {
