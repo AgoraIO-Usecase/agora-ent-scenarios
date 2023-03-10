23c53ce927d174f872a4b4cfe9577f9a3b8add95
zhaoyongqiang <zhaoyongqiang@agora.io>
2023-03-10 15:35:42 +0800

    添加资源包下载功能

---
+++ ci/build/modify_podfile.py
@@ -0,0 +1,33 @@
+import os, sys
+
+def modfiy(path, beauty_type):
+    with open(path, 'r', encoding='utf-8') as file:
+        contents = []
+        for num, line in enumerate(file):
+            line = line.strip()
+            if beauty_type == '字节':
+                line = "pod 'bytedEffect', :path => 'bytedEffect.podspec'"
+
+            elif beauty_type == '相芯' in line:
+                line = "pod 'fuLib', :path => 'fu.podspec'"
+
+            elif beauty_type == '商汤' in line:
+                line = "pod 'senseLib', :path => 'sense.podspec'"
+
+            contents.append(line)
+        file.close()
+        
+        with open(path, 'w', encoding='utf-8') as fw:
+            for content in contents:
+                if "{" in content or "}" in content:
+                    fw.write(content + "\n")
+                else:
+                    fw.write('\t'+content + "\n")
+            fw.close()
+
+
+if __name__ == '__main__':
+    print(f'argv === {sys.argv[1:]}')
+    path = sys.argv[1:][0]
+    beauty_type = eval(sys.argv[1:][1])
+    modfiy(path.strip(), beauty_type)
