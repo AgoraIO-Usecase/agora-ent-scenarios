e91a4b846aa682683031b62852e498b62a37c46a
HeZhengQing <hezhengqing@agora.io>
2023-03-14 17:11:54 +0800

    [FEAT] change scene layers

--- Android/scenes/voice/voice/src/main/java/io/agora/scene/voice/imkit/bean/ChatroomConstants.java
+++
@@ -1,9 +0,0 @@
-package io.agora.scene.voice.imkit.bean;
-
-/**
- * chatroom constant
- */
-public interface ChatroomConstants {
-    String MSG_KEY_MEMBER_ADD = "member_add";
-
-}
--- Android/scenes/voice/voice/src/main/java/io/agora/scene/voice/model/IKeepProguard.kt
+++
@@ -1,4 +0,0 @@
-package io.agora.scene.voice.model
-
-interface IKeepProguard {
-}
