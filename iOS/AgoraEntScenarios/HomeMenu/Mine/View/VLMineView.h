edf47ac1e0c762ca17f4111b718646f333f76a30
HeZhengQing <hezhengqing@agora.io>
2023-03-16 14:23:28 +0800

    [FEAT] debug mode trigger

--- iOS/AgoraEntScenarios/HomeMenu/Mine/View/VLMineView.h
+++ iOS/AgoraEntScenarios/HomeMenu/Mine/View/VLMineView.h
@@ -14,7 +14,6 @@ typedef enum : NSUInteger {
     VLMineViewClickTypeAboutUS,            // 关于我们
     VLMineViewClickTypeLogout,             // 退出登录
     VLMineViewClickTypeDestroyAccount,     // 注销账号
-    VLMineViewClickTypeDebug,              // 开发者模式
 } VLMineViewClickType;
 
 typedef enum : NSUInteger {
