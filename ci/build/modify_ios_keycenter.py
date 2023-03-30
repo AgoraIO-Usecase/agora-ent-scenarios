f63cbb132c89b5e0085c97efe906188b15b926e9
zhaoyongqiang <zhaoyongqiang@agora.io>
2023-03-30 18:48:51 +0800

    update keycenter

--- ci/build/modify_ios_keycenter.py
+++ ci/build/modify_ios_keycenter.py
@@ -3,6 +3,9 @@ import os, sys
 def modfiy(path, isReset):
     appId = os.environ.get('APP_ID')
     cert = os.environ.get('APP_CERT')
+    im_app_key = os.environ.get('IM_APP_KEY')
+    im_client_id = os.environ.get('IM_CLIENT_ID')
+    im_client_secret = os.environ.get('IM_CLIENT_SECRET')
     with open(path, 'r', encoding='utf-8') as file:
         contents = []
         for num, line in enumerate(file):
@@ -17,7 +20,28 @@ def modfiy(path, isReset):
                     line = "static let Certificate: String? = <#YOUR Certificate#>"
                 else:
                     value = cert if len(cert) > 0 else 'nil'
-                    line = 'static let Certificate: String? = "{value}"'
+                    line = f'static let Certificate: String? = "{value}"'
+
+            elif "static var IMAppKey" in line:
+                if isReset:
+                    line = "static var IMAppKey: String? = <#YOUR IMAppKey#>"
+                else:
+                    value = im_app_key if len(im_app_key) > 0 else 'nil'
+                    line = f'static var IMAppKey: String? = "{value}"'
+
+            elif "static var IMClientId" in line:
+                if isReset:
+                    line = "static var IMClientId: String? = <#YOUR IMClientId#>"
+                else:
+                    value = im_client_id if len(im_client_id) > 0 else 'nil'
+                    line = f'static var IMClientId: String? = "{value}"'
+
+            elif "static var IMClientSecret" in line:
+                if isReset:
+                    line = "static var IMClientSecret: String? = <#YOUR IMClientSecret#>"
+                else:
+                    value = im_client_secret if len(im_client_secret) > 0 else 'nil'
+                    line = f'static var IMClientSecret: String? = "{value}"'
 
             contents.append(line)
         file.close()
