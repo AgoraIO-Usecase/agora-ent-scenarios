9060fbbdc4c7f27e804f49ef4a17ee5b330b8351
zhaoyongqiang <zhaoyongqiang@agora.io>
2023-04-03 10:39:35 +0800

    fix kick off user bug

--- iOS/AgoraEntScenarios/Scenes/SpatialAudio/Views/VoiceChat/SA3DUserCollectionViewCell.swift
+++ iOS/AgoraEntScenarios/Scenes/SpatialAudio/Views/VoiceChat/SA3DUserCollectionViewCell.swift
@@ -102,7 +102,8 @@ class SA3DUserCollectionViewCell: UICollectionViewCell {
             rtcUserView.iconView.image = UIImage(mic.member?.portrait ?? "")
         }
         rtcUserView.nameBtn.setImage(UIImage((mic.mic_index == 0 || mic.mic_index == 3 || mic.mic_index == 6) ? "Landlord" : ""), for: .normal)
-        rtcUserView.nameBtn.setTitle(mic.member?.name ?? "\(mic.mic_index)", for: .normal)
+        let title = mic.status == -1 ? "\(mic.mic_index)" : (mic.member?.name ?? "\(mic.mic_index)")
+        rtcUserView.nameBtn.setTitle(title, for: .normal)
     }
 
     public func refreshVolume(vol: Int) {
