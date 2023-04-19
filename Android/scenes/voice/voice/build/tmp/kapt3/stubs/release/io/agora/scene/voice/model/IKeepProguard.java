4f6d2ae6d916b833ef491a369db851a4f1094c40
CYG <cyg@CYGdeMacBook-Pro-2.local>
2023-04-19 11:57:58 +0800

    remove useless files

--- Android/scenes/voice/voice/build/tmp/kapt3/stubs/release/io/agora/scene/voice/model/IKeepProguard.java
+++
@@ -1,7 +0,0 @@
-package io.agora.scene.voice.model;
-
-import java.lang.System;
-
-@kotlin.Metadata(mv = {1, 6, 0}, k = 1, d1 = {"\u0000\n\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\bf\u0018\u00002\u00020\u0001\u00a8\u0006\u0002"}, d2 = {"Lio/agora/scene/voice/model/IKeepProguard;", "", "voice_release"})
-public abstract interface IKeepProguard {
-}
