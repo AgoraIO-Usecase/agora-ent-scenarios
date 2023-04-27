c37e1599ed3f1ff5db58377169826ec44d89eab6
zhangwei <zw0872@163.com>
2023-04-27 16:25:51 +0800

    [Fix][Android] show music volume not working FT-441

--- Android/scenes/show/src/main/java/io/agora/scene/show/LiveDetailFragment.kt
+++ Android/scenes/show/src/main/java/io/agora/scene/show/LiveDetailFragment.kt
@@ -657,7 +657,7 @@ class LiveDetailFragment : Fragment() {
     }
 
     private fun showAdvanceSettingDialog() {
-        AdvanceSettingDialog(requireContext()).apply {
+        AdvanceSettingDialog(requireContext(), mMainRtcConnection).apply {
             setItemShowTextOnly(AdvanceSettingDialog.ITEM_ID_SWITCH_QUALITY_ENHANCE, true)
             setItemShowTextOnly(AdvanceSettingDialog.ITEM_ID_SWITCH_BITRATE_SAVE, true)
             show()
