66616e4c5faebfa11484c255828a0ce427b8da65
zhangwei <zw0872@163.com>
2023-05-18 17:38:03 +0800

    [Android]update person collection url

--- Android/app/src/main/java/com/agora/entfulldemo/home/HomeMineFragment.java
+++ Android/app/src/main/java/com/agora/entfulldemo/home/HomeMineFragment.java
@@ -92,7 +92,9 @@ public class HomeMineFragment extends BaseViewBindingFragment<AppFragmentHomeMin
             PagePilotManager.pageWebView("https://accktvpic.oss-cn-beijing.aliyuncs.com/pic/meta/demo/fulldemoStatic/privacy/privacy.html");
         });
         getBinding().tvCollectionChecklist.setOnClickListener(view -> {
-            StringBuilder stringBuilder =  new StringBuilder("https://webdemo.agora.io/agora-ent-scenarios-dev")
+            //开发服：http://fullapp.oss-cn-beijing.aliyuncs.com/ent-scenarios/pages/manifest-dev/index.html
+            //正式服：http://fullapp.oss-cn-beijing.aliyuncs.com/ent-scenarios/pages/manifest/index.html
+            StringBuilder stringBuilder =  new StringBuilder("http://fullapp.oss-cn-beijing.aliyuncs.com/ent-scenarios/pages/manifest-dev/index.html")
                     .append("?userNo=").append(UserManager.getInstance().getUser().userNo)
                     .append("&appId=").append(io.agora.scene.base.BuildConfig.AGORA_APP_ID)
                     .append("&projectId=").append("agora_ent_demo")
