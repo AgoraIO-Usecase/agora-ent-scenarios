c87e5c84e926502ccd82686038af303f669bfca1
alienzh <zw0872@163.com>
2023-05-19 11:51:52 +0800

    Feat/scene/all android 3.0.1 (#541)
    * [Android]update person collection url

--- Android/common/base/src/main/java/io/agora/scene/base/api/ApiManager.java
+++ Android/common/base/src/main/java/io/agora/scene/base/api/ApiManager.java
@@ -1,10 +1,13 @@
 package io.agora.scene.base.api;
 
+import android.os.Build;
 import android.text.TextUtils;
 import android.util.ArrayMap;
+import android.util.Log;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.moczul.ok2curl.CurlInterceptor;
 
 import java.io.File;
 import java.util.concurrent.TimeUnit;
@@ -17,6 +20,7 @@ import io.agora.scene.base.api.model.User;
 import io.agora.scene.base.bean.CommonBean;
 import io.agora.scene.base.manager.UserManager;
 import io.reactivex.Observable;
+import io.reactivex.Single;
 import okhttp3.MediaType;
 import okhttp3.MultipartBody;
 import okhttp3.OkHttpClient;
@@ -39,7 +43,7 @@ public class ApiManager {
                     .disableHtmlEscaping()
                     .registerTypeAdapter(String.class, new GsonUtils.StringConverter()).create();
         }
-        httpClient = new OkHttpClient.Builder().addInterceptor(chain -> {
+        OkHttpClient.Builder httpClientBuilder = new OkHttpClient.Builder().addInterceptor(chain -> {
                     Request.Builder builder = chain.request().newBuilder();
                     builder.addHeader(NetConstants.HEADER_APP_OS, "android");
                     builder.addHeader(NetConstants.HEADER_VERSION_NAME, BuildConfig.APP_VERSION_NAME);
@@ -59,8 +63,11 @@ public class ApiManager {
                 .addInterceptor(new HttpLoggingInterceptor())
                 .connectTimeout(TIMEOUT, TimeUnit.SECONDS)
                 .readTimeout(TIMEOUT, TimeUnit.SECONDS)
-                .writeTimeout(TIMEOUT, TimeUnit.SECONDS)
-                .build();
+                .writeTimeout(TIMEOUT, TimeUnit.SECONDS);
+        if (BuildConfig.DEBUG) {
+            httpClientBuilder.addInterceptor(new CurlInterceptor(s -> Log.d("CurlInterceptor", s)));
+        }
+        httpClient = httpClientBuilder.build();
 
 
         Retrofit sRetrofit = new Retrofit.Builder()
@@ -143,6 +150,31 @@ public class ApiManager {
             params.put("userNo", userNo);
         }
         return apiManagerService.requestUserUpdate(getRequestBody(params)).flatMap(it -> Observable.just(it));
+    }
+
+    public Observable<BaseResponse<String>> requestReportDevice(String userNo, String sceneId) {
+        ArrayMap<String, String> params = new ArrayMap();
+        //todo 这几个参数query?
+//        params.put("userNo", userNo);
+//        params.put("sceneId", sceneId);
+//        params.put("appId", BuildConfig.AGORA_APP_ID);
+//        params.put("projectId", "agora_ent_demo");
+        params.put("appVersion", BuildConfig.APP_VERSION_NAME);
+        params.put("platform", "Android");
+        params.put("model", Build.MODEL);
+        params.put("manufacture", Build.MANUFACTURER);
+        params.put("osVersion", Build.VERSION.RELEASE);
+        return apiManagerService.requestReportDevice(userNo, sceneId, BuildConfig.AGORA_APP_ID, "agora_ent_demo",
+                getRequestBody(params)).flatMap(it -> Observable.just(it));
+
+    }
+
+    public Observable<BaseResponse<String>> requestReportAction(String userNo, String action) {
+        ArrayMap<String, String> params = new ArrayMap();
+        params.put("action", action);
+        return apiManagerService.requestReportAction(userNo, action, BuildConfig.AGORA_APP_ID, "agora_ent_demo",
+                getRequestBody(params)).flatMap(it -> Observable.just(it));
+
     }
 
     private RequestBody getRequestBody(ArrayMap<String, String> params) {
