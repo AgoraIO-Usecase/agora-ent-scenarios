5491bebaadb8fb6a5378459d7fa26b685e671f04
zhaoyongqiang <zhaoyongqiang@agora.io>
2023-06-30 17:09:51 +0800

    隐藏非语聊场景

--- iOS/AgoraEntScenarios/HomeMenu/Home/VLHomeView.m
+++ iOS/AgoraEntScenarios/HomeMenu/Home/VLHomeView.m
@@ -95,18 +95,18 @@
                 @"titleStr":AGLocalizedString(@"语聊房"),
                 @"subTitleStr":AGLocalizedString(@"-空间音频模式")
             },
-            @{
-                @"bgImgStr":@"home_KTV_bg",
-                @"iconImgStr":@"home_KTV_icon",
-                @"titleStr":AGLocalizedString(@"在线K歌房"),
-                @"subTitleStr":@""
-            },
-            @{
-                @"bgImgStr":@"home_live_bg",
-                @"iconImgStr":@"home_live_icon",
-                @"titleStr":AGLocalizedString(@"秀场直播"),
-                @"subTitleStr":@""
-            },
+//            @{
+//                @"bgImgStr":@"home_KTV_bg",
+//                @"iconImgStr":@"home_KTV_icon",
+//                @"titleStr":AGLocalizedString(@"在线K歌房"),
+//                @"subTitleStr":@""
+//            },
+//            @{
+//                @"bgImgStr":@"home_live_bg",
+//                @"iconImgStr":@"home_live_icon",
+//                @"titleStr":AGLocalizedString(@"秀场直播"),
+//                @"subTitleStr":@""
+//            },
         ];
     }
     return _itemsArray;
