4c4d13e486c925e9886629905c4bb98504da19bb
wushengtao <tamworth@163.com>
2023-07-19 21:19:28 +0800

    feat: add pure 1v1 list icon

---
+++ iOS/AgoraEntScenarios/Scenes/Pure1v1/Pure1v1/Classes/Core/UIImage+Pure1v1.swift
@@ -0,0 +1,61 @@
+//
+//  UIImage+Pure1v1.swift
+//  AFNetworking
+//
+//  Created by wushengtao on 2023/7/19.
+//
+
+import UIKit
+
+func getLang() -> String {
+    guard let lang = NSLocale.preferredLanguages.first else {
+        return "en"
+    }
+
+    if lang.contains("zh") {
+        return "zh-Hans"
+    }
+
+    return "en"
+}
+
+extension UIImage {
+    static func sceneImagePath(name: String, bundleName: String) -> String? {
+        guard let bundlePath = Bundle.main.path(forResource: bundleName, ofType: "bundle"),
+              let bundle = Bundle(path: bundlePath)
+        else {
+            assertionFailure("image bundle == nil")
+            return nil
+        }
+
+        let components = name.components(separatedBy: ".")
+        let pureName = components.first ?? name
+        let suffix = (components.count == 2 ? components.last : nil) ?? "png"
+        let scale = Int(UIScreen.main.scale)
+        var scales = [1, 2, 3].filter { value in
+            return value != scale
+        }
+        scales.insert(scale, at: 0)
+        
+        let lang = ""//getLang()
+        for value in scales {
+            let imageName1 = value > 1 ? "\(pureName)-\(lang)@\(value)x" : pureName
+            let imageName2 = value > 1 ? "\(pureName)@\(value)x" : pureName
+            if let path = bundle.path(forResource: imageName1, ofType: suffix) {
+                return path
+            }
+            if let path = bundle.path(forResource: imageName2, ofType: suffix) {
+                return path
+            }
+        }
+//        assertionFailure("image path(\(pureName) == nil")
+//        print("image path(\(pureName)) == nil")
+        return nil
+    }
+    
+    @objc static func sceneImage(name: String) -> UIImage? {
+        let imagePath = UIImage.sceneImagePath(name: "Image\name", bundleName: "Pure1v1")
+        let image = UIImage(contentsOfFile: imagePath ?? "")
+        return image
+    }
+}
