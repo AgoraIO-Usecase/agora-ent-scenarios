0db43cefdac009c33869ad2d8dfa546c33f8f401
HugoChaan <784028997@qq.com>
2023-08-06 19:49:34 +0800

    optimize song score logic

--- Android/scenes/ktv_relay/src/main/java/io/agora/scene/ktv/singrelay/live/song/SongModel.kt
+++ Android/scenes/ktv_relay/src/main/java/io/agora/scene/ktv/singrelay/live/song/SongModel.kt
@@ -9,7 +9,7 @@ object SongModel {
             "6805795303139450",
             "贝乐虎；土豆王国小乐队；奶糖乐团",
             "https://accpic.sd-rtn.com/pic/release/jpg/3/640_640/CASW1078064.jpg",
-            listOf(32000, 47000, 81433, 142000, 176000)
+            listOf(32448, 47059, 81433, 142051, 176573)
     )
 
     private val song2 = ChooseSongInputModel(
@@ -17,7 +17,7 @@ object SongModel {
             "6625526604232820",
             "林俊杰",
             "https://accpic.sd-rtn.com/pic/release/jpg/3/640_640/661208.jpg",
-            listOf(55000, 97000, 150000, 190000, 243000)
+            listOf(55076, 97357, 150592, 190350, 243041)
     )
 
     private val song3 = ChooseSongInputModel(
@@ -25,7 +25,7 @@ object SongModel {
             "6654550266760610",
             "薛之谦",
             "https://accpic.sd-rtn.com/pic/release/jpg/3/640_640/CJ1420004109.jpg",
-            listOf(91000, 129000, 173000, 212000, 251000)
+            listOf(91771, 129510, 173337, 212697, 251478)
     )
 
     private val song4 = ChooseSongInputModel(
@@ -33,7 +33,7 @@ object SongModel {
             "6625526611288130",
             "郭顶",
             "https://accpic.sd-rtn.com/pic/release/jpg/3/640_640/126936.jpg",
-            listOf(44000, 89000, 132000, 192000, 244000)
+            listOf(44705, 89415, 132980, 192512, 244558)
     )
 
     private val song5 = ChooseSongInputModel(
@@ -41,7 +41,7 @@ object SongModel {
             "6654550232746660",
             "阿桑",
             "https://accpic.sd-rtn.com/pic/release/jpg/3/640_640/961853.jpg",
-            listOf(57000, 76000, 130000, 148000, 210000)
+            listOf(57291, 76635, 130940, 148464, 210536)
     )
 
     private val song6 = ChooseSongInputModel(
@@ -49,7 +49,7 @@ object SongModel {
             "6625526604594370",
             "张杰",
             "https://accpic.sd-rtn.com/pic/release/jpg/3/640_640/792885.jpg",
-            listOf(46000, 81000, 124000, 159000, 207000)
+            listOf(46190, 81460, 124830, 159670, 207590)
     )
 
     private val song7 = ChooseSongInputModel(
@@ -57,7 +57,7 @@ object SongModel {
             "6654550232990700",
             "周杰伦",
             "https://accpic.sd-rtn.com/pic/release/jpg/3/640_640/961979.jpg",
-            listOf(90000, 118000, 194000, 222000, 262000)
+            listOf(90318, 118264, 194140, 222237, 262364)
     )
 
     private val song8 = ChooseSongInputModel(
@@ -65,7 +65,7 @@ object SongModel {
             "6625526604489740",
             "孙燕姿",
             "https://accpic.sd-rtn.com/pic/release/jpg/3/640_640/147907.jpg",
-            listOf(51000, 85000, 122000, 176000, 223000)
+            listOf(51394, 85587, 122259, 176428, 223973)
     )
 
     private val song9 = ChooseSongInputModel(
@@ -73,7 +73,7 @@ object SongModel {
             "6625526603305730",
             "买辣椒也用券",
             "https://accpic.sd-rtn.com/pic/release/jpg/3/640_640/385062.jpg",
-            listOf(63000, 109000, 154000, 194000, 274000)
+            listOf(63836, 109502, 154446, 194166, 274685)
     )
 
     private val song10 = ChooseSongInputModel(
@@ -81,7 +81,7 @@ object SongModel {
             "6654550267486590",
             "莫文蔚",
             "https://accpic.sd-rtn.com/pic/release/jpg/3/640_640/CJ1420010039.jpg",
-            listOf(91000, 147000, 191000, 235000, 295000)
+            listOf(91066, 147312, 191825, 235084, 295164)
     )
 
     fun getRandomGameSong() : ChooseSongInputModel {
