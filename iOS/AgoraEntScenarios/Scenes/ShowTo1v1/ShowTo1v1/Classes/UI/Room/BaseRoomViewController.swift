db21173ea647e577c50b151e080dcc640fff6fd3
wushengtao <tamworth@163.com>
2023-08-09 14:38:07 +0800

    fix: rename

---
+++ iOS/AgoraEntScenarios/Scenes/ShowTo1v1/ShowTo1v1/Classes/UI/Room/BaseRoomViewController.swift
@@ -0,0 +1,94 @@
+//
+//  BaseRoomViewController.swift
+//  ShowTo1v1
+//
+//  Created by wushengtao on 2023/8/2.
+//
+
+import UIKit
+import CallAPI
+import AgoraRtcKit
+
+class BaseRoomViewController: UIViewController {
+    var onBackClosure: (()->())?
+    var callApi: CallApiProtocol? {
+        didSet {
+            oldValue?.removeListener(listener: self)
+            oldValue?.removeRTCListener?(listener: self.realTimeView)
+            callApi?.addListener(listener: self)
+            callApi?.addRTCListener?(listener: self.realTimeView)
+        }
+    }
+    lazy var roomInfoView: RoomInfoView = RoomInfoView()
+    lazy var bigCanvasView: UIView = UIView()
+    private lazy var bottomBar: RoomBottomBar = {
+        let bar = RoomBottomBar(frame: .zero)
+        bar.delegate = self
+        return bar
+    }()
+    
+    lazy var realTimeView: ShowRealTimeDataView = {
+        let realTimeView = ShowRealTimeDataView(isLocal: true)
+        return realTimeView
+    }()
+    
+    deinit {
+        showTo1v1Print("deinit-- ShowTo1v1BaseRoomViewController")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .black
+        
+        view.addSubview(bigCanvasView)
+        
+        view.addSubview(roomInfoView)
+        view.addSubview(bottomBar)
+        view.addSubview(realTimeView)
+        
+        
+        realTimeView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(UIDevice.current.aui_SafeDistanceTop + 50)
+        }
+        
+        roomInfoView.frame = CGRect(x: 15, y: UIDevice.current.aui_SafeDistanceTop, width: 202, height: 40)
+        bigCanvasView.frame = view.bounds
+        
+        bottomBar.frame = CGRect(x: 0, y: view.aui_height - UIDevice.current.aui_SafeDistanceBottom - 50, width: view.aui_width, height: 40)
+    }
+    
+    @objc func onBackAction() {
+        dismiss(animated: false)
+        onBackClosure?()
+    }
+}
+
+extension BaseRoomViewController: RoomBottomBarDelegate {
+    func onClickSettingButton() {
+        let settingMenuVC = ShowToolMenuViewController()
+        settingMenuVC.type = ShowMenuType.idle_audience
+        settingMenuVC.delegate = self
+        present(settingMenuVC, animated: true)
+    }
+}
+
+extension BaseRoomViewController: ShowToolMenuViewControllerDelegate {
+    func onClickRealTimeDataButtonSelected(_ menu: ShowToolMenuViewController, _ selected: Bool) {
+        view.addSubview(realTimeView)
+        realTimeView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(UIDevice.current.aui_SafeDistanceTop + 50)
+        }
+    }
+}
+
+extension BaseRoomViewController: CallApiListenerProtocol {
+    func onCallStateChanged(with state: CallStateType,
+                            stateReason: CallReason,
+                            eventReason: String,
+                            elapsed: Int,
+                            eventInfo: [String : Any]) {
+    }
+}
+
---
+++ iOS/AgoraEntScenarios/Scenes/ShowTo1v1/ShowTo1v1/Classes/UI/Room/CallViewController.swift
@@ -0,0 +1,94 @@
+//
+//  CallViewController.swift
+//  ShowTo1v1
+//
+//  Created by wushengtao on 2023/7/28.
+//
+
+import Foundation
+import CallAPI
+import AgoraRtcKit
+
+class CallViewController: BaseRoomViewController {
+    var targetUser: ShowTo1v1UserInfo? {
+        didSet {
+            roomInfoView.setRoomInfo(avatar: targetUser?.avatar ?? "",
+                                     name: targetUser?.userName ?? "",
+                                     id: targetUser?.userId ?? "",
+                                     time: Int64(Date().timeIntervalSince1970 * 1000))
+        }
+    }
+    
+    private lazy var moveViewModel: MoveGestureViewModel = MoveGestureViewModel()
+    private lazy var hangupButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setImage(UIImage.sceneImage(name: "call_reject"), for: .normal)
+        button.addTarget(self, action: #selector(_hangupAction), for: .touchUpInside)
+        return button
+    }()
+    
+    lazy var smallCanvasView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(hexString: "#0038ff")?.withAlphaComponent(0.7)
+        view.addGestureRecognizer(moveViewModel.gesture)
+        return view
+    }()
+    
+    private lazy var bottomBar: RoomBottomBar = {
+        let bar = RoomBottomBar(frame: .zero)
+        bar.delegate = self
+        return bar
+    }()
+    
+    deinit {
+        showTo1v1Print("deinit-- Pure1v1CallViewController")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(smallCanvasView)
+        
+        moveViewModel.touchArea = view.bounds
+        bigCanvasView.frame = view.bounds
+        smallCanvasView.frame = CGRect(x: view.aui_width - 25 - 109, y: 82 + UIDevice.current.aui_SafeDistanceTop, width: 109, height: 163)
+        smallCanvasView.layer.cornerRadius = 20
+        smallCanvasView.clipsToBounds = true
+    }
+    
+    @objc private func _hangupAction() {
+        callApi?.hangup(roomId: targetUser?.userId ?? "", completion: { err in
+        })
+    }
+}
+
+extension CallViewController: AgoraRtcEngineDelegate {
+    func rtcEngine(_ engine: AgoraRtcEngineKit, didOccurWarning warningCode: AgoraWarningCode) {
+        showTo1v1Warn("rtcEngine warningCode == \(warningCode.rawValue)")
+    }
+    func rtcEngine(_ engine: AgoraRtcEngineKit, didOccurError errorCode: AgoraErrorCode) {
+        showTo1v1Warn("rtcEngine errorCode == \(errorCode.rawValue)")
+    }
+    
+    public func rtcEngine(_ engine: AgoraRtcEngineKit, didJoinedOfUid uid: UInt, elapsed: Int) {
+        showTo1v1Warn("didJoinedOfUid: \(uid) elapsed: \(elapsed)")
+    }
+}
+
+extension CallViewController {
+    override func onCallStateChanged(with state: CallStateType,
+                                stateReason: CallReason,
+                                eventReason: String,
+                                elapsed: Int,
+                                eventInfo: [String : Any]) {
+    }
+    
+    func onCallEventChanged(with event: CallEvent, elapsed: Int) {
+        showTo1v1Warn("onCallEventChanged: \(event.rawValue)")
+        switch event {
+        case .localLeave, .remoteLeave:
+            _hangupAction()
+        default:
+            break
+        }
+    }
+}
