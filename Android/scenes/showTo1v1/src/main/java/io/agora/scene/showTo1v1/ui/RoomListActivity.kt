86d632189f7308c6b7a445352b599fa6c64db940
zhangwei <zw0872@163.com>
2023-08-15 20:46:34 +0800

    [ShowTo1v1] add  room list empty ui

--- Android/scenes/showTo1v1/src/main/java/io/agora/scene/showTo1v1/ui/RoomListActivity.kt
+++ Android/scenes/showTo1v1/src/main/java/io/agora/scene/showTo1v1/ui/RoomListActivity.kt
@@ -3,12 +3,12 @@ package io.agora.scene.showTo1v1.ui
 import android.os.Bundle
 import android.view.LayoutInflater
 import io.agora.scene.base.component.BaseViewBindingActivity
-import io.agora.scene.showTo1v1.databinding.Show1v1RoomListActivityBinding
+import io.agora.scene.showTo1v1.databinding.ShowTo1v1RoomListActivityBinding
 
-class RoomListActivity : BaseViewBindingActivity<Show1v1RoomListActivityBinding>() {
+class RoomListActivity : BaseViewBindingActivity<ShowTo1v1RoomListActivityBinding>() {
 
-    override fun getViewBinding(inflater: LayoutInflater): Show1v1RoomListActivityBinding {
-        return Show1v1RoomListActivityBinding.inflate(inflater)
+    override fun getViewBinding(inflater: LayoutInflater): ShowTo1v1RoomListActivityBinding {
+        return ShowTo1v1RoomListActivityBinding.inflate(inflater)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
