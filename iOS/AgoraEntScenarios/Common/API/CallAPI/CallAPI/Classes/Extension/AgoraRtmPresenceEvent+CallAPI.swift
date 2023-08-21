728887ffe97d714752cfbbe3ebd89175a0f6874e
wushengtao <tamworth@163.com>
2023-08-21 11:57:25 +0800

    fix: upgrade callapi

--- iOS/AgoraEntScenarios/Common/API/CallAPI/CallAPI/Classes/Extension/AgoraRtmPresenceEvent+CallAPI.swift
+++ iOS/AgoraEntScenarios/Common/API/CallAPI/CallAPI/Classes/Extension/AgoraRtmPresenceEvent+CallAPI.swift
@@ -11,10 +11,8 @@ let kUserIdKey = "userId"
 extension AgoraRtmPresenceEvent {
     func snapshotList() -> [[String: String]] {
         var userList: [[String: String]] = [[String: String]]()
-        let snapshotList = snapshot as NSArray
-        snapshotList.forEach { user in
+        snapshot.forEach { user in
             var userMap: [String: String] = [:]
-            let user = user as! AgoraRtmUserState
             userMap[kUserIdKey] = user.userId
             user.states.forEach { item in
                 userMap[item.key] = item.value
