1ed18dd7837d3c62f7c7ced1fc02c30352531aa2
wushengtao <tamworth@163.com>
2023-09-21 17:46:17 +0800

    feat: upgrade callapi to replace subscribe roomid with uid

--- iOS/AgoraEntScenarios/Common/API/CallAPI/CallAPI.podspec
+++ iOS/AgoraEntScenarios/Common/API/CallAPI/CallAPI.podspec
@@ -43,5 +43,5 @@ TODO: Add long description of the pod here.
 
   # s.public_header_files = 'Pod/Classes/**/*.h'
   s.dependency 'AgoraRtcEngine_Special_iOS'
-  s.dependency 'AgoraRtm_iOS', '~>2.1.4'
+  s.dependency 'AgoraRtm_iOS', '2.1.4'
 end
--- iOS/AgoraEntScenarios/Scenes/Pure1v1/Pure1v1/Classes/UI/UserList/Pure1v1UserListViewController.swift
+++ iOS/AgoraEntScenarios/Scenes/Pure1v1/Pure1v1/Classes/UI/UserList/Pure1v1UserListViewController.swift
@@ -229,7 +229,7 @@ extension Pure1v1UserListViewController: CallApiListenerProtocol {
             let toUserId = eventInfo[kRemoteUserId] as? UInt ?? 0
             pure1v1Print("calling: fromUserId: \(fromUserId) fromRoomId: \(fromRoomId) currentId: \(currentUid) toUserId: \(toUserId)")
             if let connectedUserId = connectedUserId, connectedUserId != fromUserId {
-                callApi.reject(roomId: fromRoomId, remoteUserId: fromUserId, reason: "already calling") { err in
+                callApi.reject(remoteUserId: fromUserId, reason: "already calling") { err in
                 }
                 return
             }
@@ -281,7 +281,7 @@ extension Pure1v1UserListViewController: CallApiListenerProtocol {
                     }
                     
                     dialog?.rejectClosure = { [weak self] in
-                        self?.callApi.reject(roomId: fromRoomId, remoteUserId: fromUserId, reason: "reject by user") {err in
+                        self?.callApi.reject(remoteUserId: fromUserId, reason: "reject by user") {err in
                         }
                     }
                     
