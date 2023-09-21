57cf87b8c9bc059debdd82934dcc8ea5adb03989
wushengtao <tamworth@163.com>
2023-09-21 19:04:40 +0800

    fix: dispatch group leave  calls twice fixed

--- iOS/AgoraEntScenarios/Scenes/ShowTo1v1/ShowTo1v1/Classes/Service/ShowTo1v1ServiceImp.swift
+++ iOS/AgoraEntScenarios/Scenes/ShowTo1v1/ShowTo1v1/Classes/Service/ShowTo1v1ServiceImp.swift
@@ -39,6 +39,7 @@ class ShowTo1v1ServiceImp: NSObject {
     private var userList: [ShowTo1v1UserInfo] = []
     
     private var refreshRoomListClosure: (([ShowTo1v1RoomInfo])->Void)?
+    private var joinRoomClosure: ((Error?)->())?
     
     convenience init(appId: String, user: ShowTo1v1UserInfo?) {
         self.init()
@@ -168,6 +169,7 @@ extension ShowTo1v1ServiceImp: ShowTo1v1ServiceProtocol {
     
     func joinRoom(roomInfo:ShowTo1v1RoomInfo, completion: @escaping (Error?) -> Void) {
         let reqId = NSString.withUUID()
+        self.joinRoomClosure = completion
         initScene(reqId) {[weak self] rid, error in
             guard reqId == rid else {return}
             if let error = error {
@@ -188,18 +190,21 @@ extension ShowTo1v1ServiceImp: ShowTo1v1ServiceProtocol {
                         }
                         self._subscribeUsersChanged(channelName: roomInfo.roomId)
                         self.subscribeRoomStatusChanged(channelName: roomInfo.roomId)
-                        completion(nil)
+                        self.joinRoomClosure?(nil)
+                        self.joinRoomClosure = nil
                     }
-                } fail: { error in
+                } fail: {[weak self] error in
                     showTo1v1Print("joinRoom fail2: \(error.localizedDescription)")
                     mainTreadTask {
-                        completion(error)
+                        self?.joinRoomClosure?(error)
+                        self?.joinRoomClosure = nil
                     }
                 }
-            }) { error in
+            }) {[weak self] error in
                 showTo1v1Print("joinRoom fail3: \(error.localizedDescription)")
                 mainTreadTask {
-                    completion(error)
+                    self?.joinRoomClosure?(error)
+                    self?.joinRoomClosure = nil
                 }
             }
         }
