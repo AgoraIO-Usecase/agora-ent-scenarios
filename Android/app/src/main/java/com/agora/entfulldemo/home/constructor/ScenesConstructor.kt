3c4bc1347ecbfcb2b2c22330bd1dd5a4413a313e
Hugo Chan <47882165+HugoChaan@users.noreply.github.com>
2023-09-26 15:04:28 +0800

    Feat/scene/all android 3.0.1 (#562)
    v3.0.2
    show v3.2.0

--- Android/app/src/main/java/com/agora/entfulldemo/home/constructor/ScenesConstructor.kt
+++ Android/app/src/main/java/com/agora/entfulldemo/home/constructor/ScenesConstructor.kt
@@ -2,7 +2,6 @@ package com.agora.entfulldemo.home.constructor
 
 import android.content.Context
 import com.agora.entfulldemo.R
-import io.agora.scene.base.AgoraScene
 
 /**
  * @author create by zhangwei03
@@ -13,33 +12,33 @@ object ScenesConstructor {
     @JvmStatic
     fun buildData(context: Context): List<ScenesModel> {
         return mutableListOf(
+//            ScenesModel(
+//                AgoraScenes.ChatRoom,
+//                "io.agora.scene.voice.ui.activity.VoiceRoomListActivity",
+//                context.getString(R.string.app_voice_chat),
+//                R.mipmap.bg_btn_home3,
+//                R.mipmap.bg_btn_home_chat,
+//                true
+//            ),
+//            ScenesModel(
+//                AgoraScenes.SpatialAudioChatRoom,
+//                "io.agora.scene.voice.spatial.ui.activity.VoiceRoomListActivity",
+//                context.getString(R.string.app_voice_chat),
+//                R.mipmap.bg_btn_home5,
+//                0,
+//                true,
+//                context.getString(R.string.app_voice_chat_spatialTip)
+//            ),
+//            ScenesModel(
+//                AgoraScenes.KTV,
+//                "io.agora.scene.ktv.create.RoomListActivity",
+//                context.getString(R.string.ktv_online),
+//                R.mipmap.bg_btn_home1,
+//                R.mipmap.bg_btn_home_ktv,
+//                true
+//            ),
             ScenesModel(
-                AgoraScene.ChatRoom,
-                "io.agora.scene.voice.ui.activity.VoiceRoomListActivity",
-                context.getString(R.string.app_voice_chat),
-                R.mipmap.bg_btn_home3,
-                R.mipmap.bg_btn_home_chat,
-                true
-            ),
-            ScenesModel(
-                AgoraScene.SpatialAudioChatRoom,
-                "io.agora.scene.voice.spatial.ui.activity.VoiceRoomListActivity",
-                context.getString(R.string.app_voice_chat),
-                R.mipmap.bg_btn_home5,
-                0,
-                true,
-                context.getString(R.string.app_voice_chat_spatialTip)
-            ),
-            ScenesModel(
-                AgoraScene.KTV,
-                "io.agora.scene.ktv.create.RoomListActivity",
-                context.getString(R.string.ktv_online),
-                R.mipmap.bg_btn_home1,
-                R.mipmap.bg_btn_home_ktv,
-                true
-            ),
-            ScenesModel(
-                AgoraScene.LiveShow,
+                AgoraScenes.LiveShow,
                 "io.agora.scene.show.RoomListActivity",
                 context.getString(R.string.app_show_live),
                 R.mipmap.bg_btn_home2,
