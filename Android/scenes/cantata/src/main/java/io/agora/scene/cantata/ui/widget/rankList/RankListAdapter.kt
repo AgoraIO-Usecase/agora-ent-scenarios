c82207f9584bb645bfb9567bb55f50304222fb1d
zhangwei <zw0872@163.com>
2023-10-08 15:08:25 +0800

    add rank list view

---
+++ Android/scenes/cantata/src/main/java/io/agora/scene/cantata/ui/widget/rankList/RankListAdapter.kt
@@ -0,0 +1,60 @@
+package io.agora.scene.cantata.ui.widget.rankList
+
+import android.view.View
+import io.agora.scene.base.GlideApp
+import io.agora.scene.cantata.R
+import io.agora.scene.cantata.databinding.CantataItemRankListBinding
+import io.agora.scene.widget.basic.BindingSingleAdapter
+import io.agora.scene.widget.basic.BindingViewHolder
+import io.agora.scene.widget.utils.CenterCropRoundCornerTransform
+
+class RankListAdapter : BindingSingleAdapter<RankItem, CantataItemRankListBinding>() {
+    override fun onBindViewHolder(holder: BindingViewHolder<CantataItemRankListBinding>, position: Int) {
+        val item = getItem(position) ?: return
+        val mBinding = holder.binding
+        when (position) {
+            0 -> {
+                mBinding.itemRoot.setBackgroundResource(R.drawable.cantata_game_rank_list_1_background)
+                mBinding.ivRoundRank.setBackgroundResource(R.drawable.cantata_rank_one)
+                mBinding.ivRoundRank.visibility = View.VISIBLE
+                mBinding.tvRoundRank.visibility = View.INVISIBLE
+            }
+
+            1 -> {
+                mBinding.itemRoot.setBackgroundResource(R.drawable.cantata_game_rank_list_2_background)
+                mBinding.ivRoundRank.setBackgroundResource(R.drawable.cantata_rank_two)
+                mBinding.ivRoundRank.visibility = View.VISIBLE
+                mBinding.tvRoundRank.visibility = View.INVISIBLE
+            }
+
+            2 -> {
+                mBinding.itemRoot.setBackgroundResource(R.drawable.cantata_game_rank_list_3_background)
+                mBinding.ivRoundRank.setBackgroundResource(R.drawable.cantata_rank_three)
+                mBinding.ivRoundRank.visibility = View.VISIBLE
+                mBinding.tvRoundRank.visibility = View.INVISIBLE
+            }
+
+            else -> {
+                mBinding.itemRoot.setBackgroundResource(R.drawable.cantata_game_rank_list_default_background)
+                mBinding.tvRoundRank.text = "${(position + 1)}"
+                mBinding.ivRoundRank.visibility = View.INVISIBLE
+                mBinding.tvRoundRank.visibility = View.VISIBLE
+            }
+        }
+        mBinding.tvPlayer.text = item.userName
+        if (item.score == -1) {
+            mBinding.tvScore.text = "-"
+        } else {
+            mBinding.tvScore.text = mBinding.tvScore.context.getString(R.string.cantata_score1, item.score)
+        }
+        if (item.poster == "") {
+            mBinding.ivHeader.visibility = View.INVISIBLE
+        } else {
+            GlideApp.with(mBinding.root)
+                .load(item.poster)
+                .error(R.mipmap.userimage)
+                .transform(CenterCropRoundCornerTransform(100))
+                .into(mBinding.ivHeader)
+        }
+    }
+}
---
+++ Android/scenes/cantata/src/main/java/io/agora/scene/cantata/ui/widget/rankList/RankListView.kt
@@ -0,0 +1,58 @@
+package io.agora.scene.cantata.ui.widget.rankList
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.FrameLayout
+import androidx.core.view.isGone
+import io.agora.scene.cantata.R
+import io.agora.scene.cantata.databinding.CantataLayoutGameRankListViewBinding
+import java.text.DecimalFormat
+import java.text.NumberFormat
+
+class RankListView constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) :
+    FrameLayout(context, attrs, defStyleAttr) {
+    private lateinit var mBinding: CantataLayoutGameRankListViewBinding
+    private val mAdapter = RankListAdapter()
+
+    init {
+        init(context)
+    }
+
+    private fun init(context: Context) {
+        mBinding = CantataLayoutGameRankListViewBinding.inflate(LayoutInflater.from(context), this, true)
+        mBinding.rvRankList.adapter = mAdapter
+    }
+
+    fun resetRankList(list: List<RankItem>) {
+        val newList: MutableList<RankItem> = ArrayList()
+        newList.addAll(list)
+        if (list.size < 3) {
+            for (i in 0 until 3 - list.size) {
+                val emptyItem = RankItem()
+                emptyItem.score = -1
+                emptyItem.songNum = -1
+                emptyItem.userName = context.getString(R.string.cantata_not_on_the_list)
+                emptyItem.poster = ""
+                newList.add(emptyItem)
+            }
+        }
+        mAdapter.resetAll(newList)
+        var totalScore = 0
+        list.forEach { rankItem ->
+            if (rankItem.score >= 0) {
+                totalScore += rankItem.score
+            }
+        }
+        val numberFormat: NumberFormat = DecimalFormat("#,###")
+        val formattedNumber = numberFormat.format(totalScore)
+        mBinding.tvRoundScore.text = formattedNumber
+    }
+
+    fun updateNextSong(songName: String?) {
+        mBinding.tvNextSong.isGone = songName.isNullOrEmpty()
+        songName?.let {
+            mBinding.tvNextSong.text = context.getString(R.string.cantata_next_song, it)
+        }
+    }
+}
