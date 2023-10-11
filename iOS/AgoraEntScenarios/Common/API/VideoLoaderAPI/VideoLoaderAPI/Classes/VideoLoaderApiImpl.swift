8aa1f109175bff9d49c1d811bd9eba856b10e311
Joel <31118572+HHHHex@users.noreply.github.com>
2023-10-11 12:02:47 +0800

    Dev/3.x 3.2.1merge (#573)
    merge 3.2.1

--- iOS/AgoraEntScenarios/Common/API/VideoLoaderAPI/VideoLoaderAPI/Classes/VideoLoaderApiImpl.swift
+++ iOS/AgoraEntScenarios/Common/API/VideoLoaderAPI/VideoLoaderAPI/Classes/VideoLoaderApiImpl.swift
@@ -180,7 +180,9 @@ extension VideoLoaderApiImpl: IVideoLoaderApi {
         guard let rtcEngine = self.config?.rtcEngine else {return}
         apiPrint("preloadRoom: \(preloadRoomList.count)")
         preloadRoomList.forEach { roomInfo in
-            rtcEngine.preloadChannel(byToken: roomInfo.token, channelId: roomInfo.channelName, uid: roomInfo.uid)
+            if let uid = config?.userId {
+                rtcEngine.preloadChannel(byToken: roomInfo.token, channelId: roomInfo.channelName, uid: uid)
+            }
         }
     }
     
