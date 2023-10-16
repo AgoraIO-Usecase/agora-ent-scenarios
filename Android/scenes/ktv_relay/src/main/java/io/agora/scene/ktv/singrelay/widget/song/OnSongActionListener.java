802c93811129cb0019769663e8fe78a35aa2f0db
Hugo Chan <47882165+HugoChaan@users.noreply.github.com>
2023-10-16 12:59:49 +0800

    Dev/4.0.0 merge singrelay (#585)
    merge v3.5.0 singrelay android

---
+++ Android/scenes/ktv_relay/src/main/java/io/agora/scene/ktv/singrelay/widget/song/OnSongActionListener.java
@@ -0,0 +1,7 @@
+package io.agora.scene.ktv.singrelay.widget.song;
+
+import androidx.annotation.NonNull;
+
+public interface OnSongActionListener {
+    void onStartSingRelayGame(@NonNull SongDialog dialog);
+}
