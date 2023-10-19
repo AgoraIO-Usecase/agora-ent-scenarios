875c4e51d116215eb183fe673f5f4d8a9dda72f8
zhangwei <zw0872@163.com>
2023-10-19 21:16:42 +0800

    [Feat] add find page ui logic

---
+++ Android/app/src/main/java/com/agora/entfulldemo/home/HomeFindFragment.kt
@@ -0,0 +1,122 @@
+package com.agora.entfulldemo.home
+
+import android.content.pm.ActivityInfo
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebChromeClient
+import android.webkit.WebChromeClient.CustomViewCallback
+import android.widget.FrameLayout
+import com.agora.entfulldemo.databinding.AppFragmentHomeFindBinding
+import com.agora.entfulldemo.home.constructor.URLStatics
+import io.agora.scene.base.component.BaseViewBindingFragment
+import io.agora.scene.base.component.OnFastClickListener
+import io.agora.scene.base.manager.PagePilotManager
+
+class HomeFindFragment : BaseViewBindingFragment<AppFragmentHomeFindBinding>() {
+    private var fullscreenContainer: FrameLayout? = null
+    private var customViewCallback: CustomViewCallback? = null
+    override fun getViewBinding(inflater: LayoutInflater, container: ViewGroup?): AppFragmentHomeFindBinding {
+        return AppFragmentHomeFindBinding.inflate(inflater)
+    }
+
+    override fun initView() {
+        binding.webView.loadUrl("file:///android_asset/index.html")
+    }
+
+    override fun initListener() {
+        binding.webView.webChromeClient = MyWebChromeClient()
+        binding.cvKtvInner.setOnClickListener(object : OnFastClickListener() {
+            override fun onClickJacking(view: View) {
+                PagePilotManager.pageWebView(URLStatics.findScenarioKtvURL)
+            }
+        })
+        binding.tvKtvDoc.setOnClickListener(object : OnFastClickListener() {
+            override fun onClickJacking(view: View) {
+                PagePilotManager.pageWebView(URLStatics.findDocKtvURL)
+            }
+        })
+        binding.btnSearchMusic.setOnClickListener(object : OnFastClickListener() {
+            override fun onClickJacking(view: View) {
+                PagePilotManager.pageWebView(URLStatics.findKtvSearchURL)
+            }
+        })
+        binding.cvChatroom.setOnClickListener(object : OnFastClickListener() {
+            override fun onClickJacking(view: View) {
+                PagePilotManager.pageWebView(URLStatics.findScenarioChatroomURL)
+            }
+        })
+        binding.tvChatroomDoc.setOnClickListener(object : OnFastClickListener() {
+            override fun onClickJacking(view: View) {
+                PagePilotManager.pageWebView(URLStatics.findDocChatroomURL)
+            }
+        })
+        binding.cvLive.setOnClickListener(object : OnFastClickListener() {
+            override fun onClickJacking(view: View) {
+                PagePilotManager.pageWebView(URLStatics.findScenarioLiveURL)
+            }
+        })
+        binding.tvLiveDoc.setOnClickListener(object : OnFastClickListener() {
+            override fun onClickJacking(view: View) {
+                PagePilotManager.pageWebView(URLStatics.findDocLiveURL)
+            }
+        })
+        binding.cvAiDenoise.setOnClickListener(object : OnFastClickListener() {
+            override fun onClickJacking(view: View) {
+                PagePilotManager.pageWebView(URLStatics.findAIDenoiseURL)
+            }
+        })
+        binding.cvAiSpatial.setOnClickListener(object : OnFastClickListener() {
+            override fun onClickJacking(view: View) {
+                PagePilotManager.pageWebView(URLStatics.findAISpatialURL)
+            }
+        })
+    }
+
+    private inner class MyWebChromeClient : WebChromeClient() {
+        override fun onHideCustomView() {
+            //退出全屏
+            hideCustomView()
+        }
+
+        override fun onShowCustomView(view: View, callback: CustomViewCallback) {
+            //进入全屏
+            showCustomView(view, callback)
+        }
+    }
+
+    /**
+     * 显示自定义控件
+     */
+    private fun showCustomView(view: View, callback: CustomViewCallback) {
+        if (fullscreenContainer != null) {
+            callback.onCustomViewHidden()
+            return
+        }
+        fullscreenContainer = FrameLayout(requireActivity())
+        fullscreenContainer!!.setBackgroundColor(Color.WHITE)
+        customViewCallback = callback
+        fullscreenContainer!!.addView(view)
+        val decorView = requireActivity().window.decorView as FrameLayout
+        // 收到需求，WebView不允许横屏
+        requireActivity().requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        decorView.addView(fullscreenContainer)
+    }
+
+    /**
+     * 隐藏自定义控件
+     */
+    private fun hideCustomView() {
+        if (fullscreenContainer == null) {
+            return
+        }
+        val decorView = requireActivity().window.decorView as FrameLayout
+        requireActivity().requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        fullscreenContainer!!.removeAllViews()
+        decorView.removeView(fullscreenContainer)
+        fullscreenContainer = null
+        customViewCallback!!.onCustomViewHidden()
+        customViewCallback = null
+    }
+}
--- Android/app/src/main/java/com/agora/entfulldemo/home/constructor/URLStatics.kt
+++ Android/app/src/main/java/com/agora/entfulldemo/home/constructor/URLStatics.kt
@@ -8,5 +8,15 @@ class URLStatics {
         //正式服：http://fullapp.oss-cn-beijing.aliyuncs.com/ent-scenarios/pages/manifest/index.html
         const val collectionChecklistURL = "http://fullapp.oss-cn-beijing.aliyuncs.com/ent-scenarios/pages/manifest/index.html"
         const val dataSharingURL = "https://fullapp.oss-cn-beijing.aliyuncs.com/scenarios/libraries.html"
+
+        const val findKtvSearchURL = "https://www.shengwang.cn/ktv_feedback/"
+        const val findScenarioKtvURL = "https://www.shengwang.cn/solution/online-karaoke/"
+        const val findDocKtvURL = "https://docportal.shengwang.cn/cn/online-ktv/landing-page?platform=Android"
+        const val findScenarioChatroomURL = "https://www.shengwang.cn/solution/voicechat/"
+        const val findDocChatroomURL = "https://docportal.shengwang.cn/cn/chatroom/landing-page?platform=Android"
+        const val findScenarioLiveURL = "https://www.shengwang.cn/solution/hd-video/"
+        const val findDocLiveURL = "https://docportal.shengwang.cn/cn/showroom/landing-page?platform=Android"
+        const val findAIDenoiseURL = "https://www.shengwang.cn/AI-denoiser/"
+        const val findAISpatialURL = "https://www.shengwang.cn/3D-spatial/"
     }
 }
