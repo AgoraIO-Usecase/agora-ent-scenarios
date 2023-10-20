b85aec8ba0ef50bf17a827d3d0c7bcc7d45d9e3f
zhangwei <zw0872@163.com>
2023-10-20 10:01:39 +0800

    [Feat] update privacy policy url

--- Android/app/src/main/java/com/agora/entfulldemo/home/constructor/URLStatics.kt
+++ Android/app/src/main/java/com/agora/entfulldemo/home/constructor/URLStatics.kt
@@ -3,11 +3,11 @@ package com.agora.entfulldemo.home.constructor
 class URLStatics {
     companion object {
         const val userAgreementURL = "https://accktvpic.oss-cn-beijing.aliyuncs.com/pic/meta/demo/fulldemoStatic/privacy/service.html"
-        const val privacyAgreementURL = "https://accktvpic.oss-cn-beijing.aliyuncs.com/pic/meta/demo/fulldemoStatic/privacy/privacy.html"
+        const val privacyAgreementURL = "https://fullapp.oss-cn-beijing.aliyuncs.com/scenarios/privacy.html"
         //开发服：http://fullapp.oss-cn-beijing.aliyuncs.com/ent-scenarios/pages/manifest-dev/index.html
         //正式服：http://fullapp.oss-cn-beijing.aliyuncs.com/ent-scenarios/pages/manifest/index.html
         const val collectionChecklistURL = "http://fullapp.oss-cn-beijing.aliyuncs.com/ent-scenarios/pages/manifest/index.html"
-        const val dataSharingURL = "https://fullapp.oss-cn-beijing.aliyuncs.com/scenarios/libraries.html"
+        const val thirdDataSharingURL = "https://fullapp.oss-cn-beijing.aliyuncs.com/scenarios/libraries.html"
 
         const val findKtvSearchURL = "https://www.shengwang.cn/ktv_feedback/"
         const val findScenarioKtvURL = "https://www.shengwang.cn/solution/online-karaoke/"
