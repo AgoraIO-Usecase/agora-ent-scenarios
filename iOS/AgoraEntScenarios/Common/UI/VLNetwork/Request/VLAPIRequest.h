24d0edfea22645fd218aee472bf887b52490da22
yiwangwuqianfjp <fan_jun_peng@163.com>
2023-10-30 12:34:26 +0800

    .0Dev/scene/all 4.0.0 fp (#590)
    * update screen updater
    
    * [FEAT] bit rate standard adapt & beauty api
    
    * [FIX] remove items
    
    * update logic
    
    * update logic
    
    * update logic
    
    * [FEAT] fast open 1,2
    
    * update service and protocol
    
    * [FEAT] join channel and load syncManager at the same time
    
    * update service logic
    
    * [FEAT] preload live show room
    
    * [FEAT] optimize localized string
    
    * [FEAT] abstract extension to common file
    
    * update show
    
    * [FEAT] bit rate settings
    
    * add localize
    
    * fix show localized
    
    * fix show localized left
    
    * fix show localized
    
    * 修改国际化
    
    * 修改国际化方法
    
    * fix: update localized string
    
    * [FEAT] 3.0.1 feature
    
    * [FIX] show presetting bugs
    
    * feat: add videoloader podspec
    
    * fix: api logger & setup remote video incorrect
    
    * fix: pk message unsubscribe fixed
    
    * fix: pk incorrect
    
    * fix:  video loader api recovery
    
    * fix: preload optimize
    
    (cherry picked from commit d2546fd84bc2ef65fac7d6a1ee1a6d7d23a637ad)
    
    * feat: touch to preload show room channel
    
    * fix: log format
    
    * fix: update bg image
    
    * [FEAT] show setting update
    
    * fix: render incorrect
    
    * [FIX] delete useless code
    
    * [FIX] beauty camera failure because bundle id is error
    
    * [FIX] fix some iOS bugs for test
    
    * [FIX] update app version config
    
    * fix: clean api cache
    
    (cherry picked from commit ea6de2e89a44e193820d467cb87c5b1cea912409)
    
    * [FIX] show user disconnect PK
    
    * [FIX] show 3.0.1 first fix some bugs
    
    * [FIX] fix bug delete file
    
    * [FIX] update h265 setting
    
    * [FIX] setup low bit rate
    
    * [FIX] BeautyAPI bugs
    
    * [FIX] beauty for CI
    
    * fix: lock 'sdwebimage' version
    
    * fix: update logs
    
    * fix: optimize video loader
    
    * feat: update api to add profiler & fix proxy incorrect
    
    * fix: pk display incorrect fixed
    
    * fix: optimize preload channel
    
    * fix: filter empty room list object
    
    * fix: update log
    
    * fix: update logs
    
    * fix: scroll to top = false
    
    * fix: filter empty room info
    
    * fix: add new func "visibleCellTuple"
    
    * fix: pk real time info missing fixed
    
    * [FIX] show fast open some bugs
    
    * [FIX] update base url
    
    * fix: update log
    
    * [FEAT] update toolbox base url
    
    * 修改ci
    
    * fix user count
    
    * [FEAT] create live view controller setting
    
    * fix: update ignore
    
    * [FIX] show live room bugs
    
    * fix 24fps & enter room Msg bug
    
    * Feat/scene/all ios 3.0.1 fp (#559)
    
    * fix show optimize
    
    * fix
    
    * fix user count
    
    * fix
    
    * fix finish view bug
    
    * merge 3.0.1, fix audience send log
    
    * fix language & log
    
    * delete unnecessary log
    
    * fix debug mode
    
    * fix debug mode
    
    * fix: calc first frame cost fixed
    
    * fix beaury reset
    
    * fix timestamp log
    
    * fix network
    
    * fix network
    
    * [FEAT] Android download AARs && iOS preload channel
    
    * [FEAT] android and iOS app version code
    
    * [FIX] update android app version
    
    * [FIX] code time version && beauty license
    
    * [FEAT] android beauty camera switch bug fix
    
    * fix
    
    * fix
    
    * fix debug mode bug
    
    * fix upload image
    
    * fix network
    
    * fix network
    
    * fix network use urlsession
    
    * fix upload image
    
    * fix download picture size
    
    * fix
    
    * fix image compress
    
    * fix editedImage
    
    * remove useless files
    
    * fix parse fail
    
    * fix conflict
    
    * fix voice room request
    
    * Revert "fix voice room request"
    
    This reverts commit 848bc9efe4265af1eb766e7406164d6578fae712.
    
    * fix
    
    * fix token expire
    
    * fix: update url
    
    * fix
    
    * fix add files
    
    ---------
    
    Co-authored-by: chenpark <1120627616@qq.com>
    Co-authored-by: HeZhengQing <hezhengqing@agora.io>
    Co-authored-by: zhaoyongqiang <zhaoyongqiang@agora.io>
    Co-authored-by: wushengtao <tamworth@163.com>

--- iOS/AgoraEntScenarios/Common/UI/VLNetwork/Request/VLAPIRequest.h
+++ iOS/AgoraEntScenarios/Common/UI/VLNetwork/Request/VLAPIRequest.h
@@ -24,7 +24,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 + (void)postRequestURL:(NSString *)url parameter:(id)parameter showHUD:(BOOL)show success:(completeBlock_success)success failure:(errorBlock_fail)failure;
 
-+ (void)requestRoute:(NSString *)route showHUD:(BOOL)show method:(NSString *)method parameter:(id)json requestType:(VLRequestType)type progressBlock:(progressBlock)progressBlock completeBlock:(completeBlock_success)completeBlock errorBlock:(errorBlock_fail)errorBlock;
++ (void)requestRoute:(NSString *)route showHUD:(BOOL)show method:(NSString *)method parameter:(id)json requestType:(VLRequestType)type progressBlock:(nullable progressBlock)progressBlock completeBlock:(completeBlock_success)completeBlock errorBlock:(errorBlock_fail)errorBlock;
 
 #pragma mark -- 直接获取图片
 + (void)requestImageRoute:(NSString *)route  method:(NSString *)method parameter:(id)json requestType:(VLRequestType)type progressBlock:(progressBlock)progressBlock completeBlock:(completeImageBlock_success)completeBlock errorBlock:(errorBlock_fail)errorBlock;
@@ -59,7 +59,7 @@ NS_ASSUME_NONNULL_BEGIN
                                          fileNames:(NSArray<NSString *> *)fileNames
                                         imageScale:(CGFloat)imageScale
                                          imageType:(NSString *)imageType
-                                          progressBlock:(progressBlock)progressBlock
+                                          progressBlock:(nullable progressBlock)progressBlock
                                            completeBlock:(completeBlock_success)completeBlock
                                            errorBlock:(errorBlock_fail)errorBlock;
 
