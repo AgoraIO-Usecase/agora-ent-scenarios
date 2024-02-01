0f2051c5350a05b5f00afaedf5fb5a0d65d989ae
wushengtao <tamworth@163.com>
2024-02-01 16:53:53 +0800

    fix: keycenter migration host

--- iOS/AgoraEntScenarios/KeyCenter.swift
+++ iOS/AgoraEntScenarios/KeyCenter.swift
@@ -15,25 +15,25 @@ class KeyCenter: NSObject {
      If you have multiple completely separate apps in your organization, for example built by different teams,
      you should use different App IDs.
      If applications need to communicate with each other, they should use the same App ID.
-     In order to get the APP ID, you can open the agora console (https://console.agora.io/) to create a project,
+     In order to get the APP ID, you can open the agora console (https://console.shengwang.cn/) to create a project,
      then the APP ID can be found in the project detail page.
      声网APP ID
      Agora 给应用程序开发人员分配 App ID，以识别项目和组织。如果组织中有多个完全分开的应用程序，例如由不同的团队构建，
      则应使用不同的 App ID。如果应用程序需要相互通信，则应使用同一个App ID。
-     进入声网控制台(https://console.agora.io/)，创建一个项目，进入项目配置页，即可看到APP ID。
+     进入声网控制台(https://console.shengwang.cn/)，创建一个项目，进入项目配置页，即可看到APP ID。
      */
-
     static let AppId: String = <#Your AppId#>
+  
     /**
      Certificate.
      Agora provides App certificate to generate Token. You can deploy and generate a token on your server,
      or use the console to generate a temporary token.
-     In order to get the APP ID, you can open the agora console (https://console.agora.io/) to create a project with the App Certificate enabled,
+     In order to get the APP ID, you can open the agora console (https://console.shengwang.cn/) to create a project with the App Certificate enabled,
      then the APP Certificate can be found in the project detail page.
      PS: If the project does not have certificates enabled, leave this field blank.
      声网APP证书
      Agora 提供 App certificate 用以生成 Token。您可以在您的服务器部署并生成，或者使用控制台生成临时的 Token。
-     进入声网控制台(https://console.agora.io/)，创建一个带证书鉴权的项目，进入项目配置页，即可看到APP证书。
+     进入声网控制台(https://console.shengwang.cn/)，创建一个带证书鉴权的项目，进入项目配置页，即可看到APP证书。
      注意：如果项目没有开启证书鉴权，这个字段留空。
      */
     static let Certificate: String? = <#YOUR Certificate#>
@@ -51,7 +51,7 @@ class KeyCenter: NSObject {
 
     static var Token: String? = nil
     
-    static var HostUrl: String = "https://gateway-fulldemo.agoralab.co"
+    static var HostUrl: String = "https://gateway-fulldemo.shengwang.cn/"
     /**
      EaseMob APPKEY.
      The application name filled in when creating an application on the EaseMob  console.
@@ -94,7 +94,7 @@ class KeyCenter: NSObject {
     static let CloudPlayerKey: String? = nil
     static let CloudPlayerSecret: String? = nil
 
-    static var baseServerUrl: String? = "https://service.agora.io/toolbox/"
+    static var baseServerUrl: String? = "https://service.shengwang.cn/toolbox"
     static var onlineBaseServerUrl: String? = baseServerUrl
 
 }
