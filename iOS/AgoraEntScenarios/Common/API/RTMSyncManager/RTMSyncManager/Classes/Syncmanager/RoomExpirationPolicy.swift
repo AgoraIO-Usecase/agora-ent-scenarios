1cbf224b6f9f6f2b66ba9a34b59b1464907ad45d
wushengtao <tamworth@163.com>
2024-07-25 17:31:27 +0800

    fix: upgrade rtm syncmanager

--- iOS/AgoraEntScenarios/Common/API/RTMSyncManager/RTMSyncManager/Classes/Syncmanager/RoomExpirationPolicy.swift
+++ iOS/AgoraEntScenarios/Common/API/RTMSyncManager/RTMSyncManager/Classes/Syncmanager/RoomExpirationPolicy.swift
@@ -12,6 +12,9 @@ public class RoomExpirationPolicy: NSObject {
     // 房间过期时间，单位ms，0表示房间不过期
     public var expirationTime: UInt64 = 0
     
+    /// 房主离线最长时间，单位ms，超过该时间认为房间被销毁，0表示不处理
+    public var ownerReconnectMaxTime: UInt64 = 5 * 60 * 1000
+    
     // 是否和房主离线关联，true: 房主不在线则销毁房间，false: 房主不在线不销毁房间
     public var isAssociatedWithOwnerOffline: Bool = true
     
