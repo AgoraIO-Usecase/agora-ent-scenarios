4adef586c8b25a5eda73ad5874164d726ac4c5b4
wushengtao <tamworth@163.com>
2024-07-29 17:52:20 +0800

    fix: minor changes

--- iOS/AgoraEntScenarios/Scenes/Show/Models/ShowAgoraKitManager.swift
+++ iOS/AgoraEntScenarios/Scenes/Show/Models/ShowAgoraKitManager.swift
@@ -35,7 +35,7 @@ class ShowAgoraKitManager: NSObject {
     public var deviceLevel: DeviceLevel = .medium
     public var deviceScore: Int = 100
     public var netCondition: NetCondition = .good
-    public var performanceMode: PerformanceMode = .smooth
+    public var performanceMode: PerformanceMode = .fluent
     
     private var broadcasterConnection: AgoraRtcConnection?
     
