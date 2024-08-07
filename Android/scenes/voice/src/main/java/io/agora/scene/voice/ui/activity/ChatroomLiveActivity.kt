55bf3bbfd2cb37fe0c3e1c63d4e60384f3c518db
alienzh <zw0872@163.com>
2024-08-07 10:35:08 +0800

    [Fix:][voice room] rtm room expire，ENT-1913 (#1041)
    * [Fix:][voice room] rtm room expire，ENT-1913
    
    * [Fix:][voice room] rtm room expire，ENT-1913

--- Android/scenes/voice/src/main/java/io/agora/scene/voice/ui/activity/ChatroomLiveActivity.kt
+++ Android/scenes/voice/src/main/java/io/agora/scene/voice/ui/activity/ChatroomLiveActivity.kt
@@ -406,7 +406,7 @@ class ChatroomLiveActivity : BaseViewBindingActivity<VoiceActivityChatroomBindin
 
             override fun onSyncRoomDestroy() {
                 roomObservableDelegate.onTimeUpExitRoom(
-                    getString(R.string.voice_chatroom_time_up_tips), finishBack = {
+                    getString(R.string.voice_room_close), finishBack = {
                         if (voiceRoomModel.isOwner) {
                             leaveRoom()
                         } else {
