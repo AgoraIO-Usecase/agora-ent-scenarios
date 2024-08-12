3ea41de9a8c893ba39c3e0cd2eeb96a6424d4871
wushengtao <tamworth@163.com>
2024-08-12 12:23:50 +0800

    fix: ci script fixed

--- ci/build/build_ios.sh
+++ ci/build/build_ios.sh
@@ -107,7 +107,7 @@ download_file () {
 }
 
 # add pod cache if need
-if [[ pod_cache_url == *http* ]]; then 
+if [[ $pod_cache_url == *https://* ]]; then 
     zip_name=${pod_cache_url##*/}
     zip_file=${WORKSPACE}/$zip_name
     echo download file: $pod_cache_url
