5c9549c29418a9165c92418e8431b366ef7edfec
wushengtao <tamworth@163.com>
2024-09-03 17:51:05 +0800

    fix: remove ktv scene

--- iOS/AgoraEntScenarios/HomeMenu/Home/HomeContentViewController.swift
+++ iOS/AgoraEntScenarios/HomeMenu/Home/HomeContentViewController.swift
@@ -91,20 +91,29 @@ extension HomeContentViewController: UICollectionViewDelegate, UICollectionViewD
         
         switch model.type {
         case .solo:
-            let vc = VLOnLineListVC()
-            navigationController?.pushViewController(vc, animated: true)
+            ToastView.show(text: NSLocalizedString("app_coming_soon", comment: ""))
+            return
+//            let vc = VLOnLineListVC()
+//            navigationController?.pushViewController(vc, animated: true)
             
         case .chorus:
-            let vc = CantataPlugin.getCantataRootViewController()
-            self.navigationController?.pushViewController(vc, animated: true)
+            ToastView.show(text: NSLocalizedString("app_coming_soon", comment: ""))
+            return
+//            let vc = CantataPlugin.getCantataRootViewController()
+//            self.navigationController?.pushViewController(vc, animated: true)
             
         case .continue_singing:
-            let vc = VLSROnLineListVC()
-            navigationController?.pushViewController(vc, animated: true)
+            ToastView.show(text: NSLocalizedString("app_coming_soon", comment: ""))
+            return
+            
+//            let vc = VLSROnLineListVC()
+//            navigationController?.pushViewController(vc, animated: true)
             
         case .snatch_singing:
-            let vc = VLSBGOnLineListVC()
-            navigationController?.pushViewController(vc, animated: true)
+            ToastView.show(text: NSLocalizedString("app_coming_soon", comment: ""))
+            return
+//            let vc = VLSBGOnLineListVC()
+//            navigationController?.pushViewController(vc, animated: true)
         case .voice_chat:
             let vc = VRRoomsViewController(user: VLUserCenter.user)
             navigationController?.pushViewController(vc, animated: true)
