14e6517423fad3a34bc4c8b9d9cf529cb85fc8c9
HeZhengQing <hezhengqing@agora.io>
2024-09-18 16:05:55 +0800

    [FEAT] remove ssl check

--- Android/common/base/src/main/java/io/agora/scene/base/api/ApiManager.java
+++ Android/common/base/src/main/java/io/agora/scene/base/api/ApiManager.java
@@ -7,10 +7,20 @@ import android.util.Log;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-//import com.moczul.ok2curl.CurlInterceptor;
+import com.moczul.ok2curl.CurlInterceptor;
 
 import java.io.File;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import io.agora.scene.base.BuildConfig;
 import io.agora.scene.base.api.apiutils.GsonUtils;
@@ -20,7 +30,6 @@ import io.agora.scene.base.api.model.User;
 import io.agora.scene.base.bean.CommonBean;
 import io.agora.scene.base.manager.UserManager;
 import io.reactivex.Observable;
-import io.reactivex.Single;
 import okhttp3.MediaType;
 import okhttp3.MultipartBody;
 import okhttp3.OkHttpClient;
@@ -37,12 +46,31 @@ public class ApiManager {
     private ApiManagerService apiManagerService;
     public static String token;
 
-    private ApiManager() {
+    TrustManager[] trustAllCerts = new TrustManager[]{
+            new X509TrustManager() {
+                public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                    return new X509Certificate[]{};
+                }
+
+                @Override
+                public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+                }
+
+                @Override
+                public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+                }
+            }
+    };
+
+    private ApiManager() throws NoSuchAlgorithmException, KeyManagementException {
         if (mGson == null) {
             mGson = new GsonBuilder().serializeNulls()
                     .disableHtmlEscaping()
                     .registerTypeAdapter(String.class, new GsonUtils.StringConverter()).create();
         }
+        // 设定 SSL 上下文来忽略证书验证
+        SSLContext sslContext = SSLContext.getInstance("SSL");
+        sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
         OkHttpClient.Builder httpClientBuilder = new OkHttpClient.Builder().addInterceptor(chain -> {
                     Request.Builder builder = chain.request().newBuilder();
                     builder.addHeader(NetConstants.HEADER_APP_OS, "android");
@@ -63,7 +91,9 @@ public class ApiManager {
                 .addInterceptor(new HttpLoggingInterceptor())
                 .connectTimeout(TIMEOUT, TimeUnit.SECONDS)
                 .readTimeout(TIMEOUT, TimeUnit.SECONDS)
-                .writeTimeout(TIMEOUT, TimeUnit.SECONDS);
+                .writeTimeout(TIMEOUT, TimeUnit.SECONDS)
+                .sslSocketFactory(sslContext.getSocketFactory(), (X509TrustManager) trustAllCerts[0])
+                .hostnameVerifier((hostname, session) -> true);
         if (BuildConfig.DEBUG) {
 //            httpClientBuilder.addInterceptor(new CurlInterceptor(s -> Log.d("CurlInterceptor", s)));
         }
@@ -80,7 +110,15 @@ public class ApiManager {
     }
 
     private static class SingletonHolder {
-        private static final ApiManager INSTANCE = new ApiManager();
+        private static final ApiManager INSTANCE;
+
+        static {
+            try {
+                INSTANCE = new ApiManager();
+            } catch (NoSuchAlgorithmException | KeyManagementException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
     public static ApiManager getInstance() {
