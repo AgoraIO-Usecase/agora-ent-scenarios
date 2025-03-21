f5948d708861637f0531b5f43c21b5b034f231d2
HeZhengQing <hezhengqing@agora.io>
2025-03-21 11:43:48 +0800

    [FIX] add discovery web view controller

---
+++ iOS/AgoraEntScenarios/HomeMenu/Discovery/ViewController/VLDiscoveryWebViewController.swift
@@ -0,0 +1,16 @@
+//
+//  VLDiscoveyrController.swift
+//  AgoraEntScenarios
+//
+//  Created by zhaoyongqiang on 2023/10/20.
+//
+
+import UIKit
+
+@objc
+class VLDiscoveryWebViewController: VLCommonWebViewController {
+    
+    override func setNaviTitleName(_ titleStr: String) {
+        super.setNaviTitleName(NSLocalizedString("app_name", comment: ""))
+    }
+}
