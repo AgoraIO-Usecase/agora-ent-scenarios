8a354e9990fdc1a4d44038749cbb9fd74eec0c86
zhangwei <zhangwei03@shengwang.cn>
2025-05-28 14:40:16 +0800

    [Android] Update release.

--- ci/build/build_android.sh
+++ ci/build/build_android.sh
@@ -116,8 +116,6 @@ else
   fi
 fi
 
-# Set up Gradle directory
-[ ! -d ~/.gradle ] && mkdir -p /tmp/.gradle && ln -s /tmp/.gradle ~/.gradle && touch ~/.gradle/ln_$(date "+%y%m%d%H")
 echo "ANDROID_HOME: $ANDROID_HOME"
 
 # Ensure beauty_sources has https:// prefix
